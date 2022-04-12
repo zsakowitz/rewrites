5c198b9fa847e74623e8091255bfab3f3f584ce4
Zachary <zsakowitz@gmail.com>
2022-04-12 07:03:40 -0700

    Add imply and nimply

--- nand.ts
+++ nand.ts
@@ -5,3 +5,7 @@ let and = (a: boolean, b: boolean) => not(nand(a, b));
 let xor = (a: boolean, b: boolean) => and(or(a, b), nand(a, b));
 let xnor = (a: boolean, b: boolean) => nand(or(a, b), nand(a, b));
 let nor = (a: boolean, b: boolean) => and(not(a), not(b));
+let imply = (a: boolean, b: boolean) => nand(a, not(b));
+let nimply = (a: boolean, b: boolean) => and(a, not(b));
+
+export {};
