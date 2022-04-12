abf7acf4ea9315df5c71f6b23a5193afb8089cfc
Zachary <zsakowitz@gmail.com>
2022-04-11 21:08:49 -0700

    Add nor function

--- nand.ts
+++ nand.ts
@@ -1,6 +1,7 @@
 let nand = (a: boolean, b: boolean) => !(a && b);
 let not = (a: boolean) => nand(a, a);
-let or = (a: boolean, b: boolean) => nand(nand(a, a), nand(b, b));
+let or = (a: boolean, b: boolean) => nand(not(a), not(b));
 let and = (a: boolean, b: boolean) => not(nand(a, b));
 let xor = (a: boolean, b: boolean) => and(or(a, b), nand(a, b));
 let xnor = (a: boolean, b: boolean) => nand(or(a, b), nand(a, b));
+let nor = (a: boolean, b: boolean) => and(not(a), not(b));
