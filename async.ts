12f0be12590af90e8674816f4e9d036fd88c5de6
Zachary <zsakowitz@gmail.com>
2022-04-16 18:01:55 -0700

    Add async.ts

---
+++ async.ts
@@ -0,0 +1,21 @@
+function promisify<I extends any[], O>(
+  func: (...args: [...args: I, cb: (err: unknown, value: O) => void]) => void
+) {
+  return (...args: I) => {
+    return new Promise<O>((resolve, reject) => {
+      func(...args, (err: unknown, value: O) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(value);
+        }
+      });
+    });
+  };
+}
+
+let e = promisify((a: number, c: number, cb: (err: any, value: 23) => void) => {
+  cb(0, 23);
+});
+
+export {};
