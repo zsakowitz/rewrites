ce6a88394856e0cca0f0a6947b5fa5dfcfa06401
Zachary <zsakowitz@gmail.com>
2022-04-19 19:43:10 -0700

    Update typed-timeout

--- typed-timeout.ts
+++ typed-timeout.ts
@@ -1,7 +1,21 @@
-export function setTimeout<T extends (...args: U) => void, U extends any[]>(
+export function setTimeout<T extends (...args: any) => any>(
   handler: T,
   timeout?: number,
-  ...args: U
-) {
-  globalThis.setTimeout(handler, timeout, ...args);
+  ...args: Parameters<T>
+): number {
+  return globalThis.setTimeout(handler, timeout, ...(args as any));
 }
+
+export class Timeout<T extends (...args: any) => any> {
+  readonly id: number;
+
+  constructor(readonly handler: T, timeout: number, ...params: Parameters<T>) {
+    this.id = setTimeout(handler, timeout, ...params);
+  }
+
+  cancel() {
+    clearTimeout(this.id);
+  }
+}
+
+let t = new Timeout((a: string) => 23, 90, "abc");
