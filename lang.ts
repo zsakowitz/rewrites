83b808bdbb1b5d727308f8070bafd50e55b5a499
Zachary <zsakowitz@gmail.com>
2022-04-21 19:58:07 -0700

    Add custom startRule to toAST

--- lang.ts
+++ lang.ts
@@ -3,7 +3,8 @@ import extras from "ohm-js/extras/index.js";
 import story from "./story.ohm-bundle.js";
 
 globalThis.story = story;
-globalThis.toAST = (text: string) => extras.toAST(story.match(text));
+globalThis.toAST = (text: string, startRule?: string) =>
+  extras.toAST(story.match(text, startRule));
 
 declare global {
   var story: ohm.Grammar;
