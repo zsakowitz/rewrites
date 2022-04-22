5cfac1c2740f14ccdb82c07882cacbed94748c6c
Zachary <zsakowitz@gmail.com>
2022-04-21 19:05:31 -0700

    Rename story.ohm

--- lang.ts
+++ lang.ts
@@ -2,7 +2,7 @@ import ohm from "ohm-js";
 import extras from "ohm-js/extras/index.js";
 import { readFileSync } from "fs";
 
-let lang = readFileSync("../lang.ohm", "utf8");
+let lang = readFileSync("../story.ohm", "utf8");
 globalThis.story = ohm.grammar(lang);
 globalThis.toAST = (text: string) => extras.toAST(story.match(text));
 
