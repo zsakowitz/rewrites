cf54fc2d9beca557b0628e9b7fef0ab2407d0814
Zachary <zsakowitz@gmail.com>
2022-04-23 07:55:03 -0700

    Add more nodes

--- lang.ts
+++ lang.ts
@@ -15,10 +15,10 @@ globalThis.semantics = story.createSemantics();
 globalThis.js = (text: string) => semantics(story.match(text)).js();
 
 interface Node {
-  output: string;
-  scopedVariables: string[];
-  isAsync: boolean;
-  isGenerator: boolean;
+  readonly output: string;
+  readonly scopedVariables: string[];
+  readonly isAsync: boolean;
+  readonly isGenerator: boolean;
 }
 
 interface PartialNode extends Partial<Node> {
@@ -26,23 +26,19 @@ interface PartialNode extends Partial<Node> {
 }
 
 function createNode(options: string | PartialNode, ...parents: Node[]): Node {
-  if (typeof options == "object") parents.push(options as any);
-  else parents.push({ output: options } as any);
+  // The type assertion here ensures that the `options` node is included as a standard node.
+  if (typeof options == "object") {
+    parents.push(options as any);
+  } else {
+    parents.push({ output: options, scopedVariables: [] } as any);
+  }
 
-  return parents.reduce<Node>(
-    (a, b) => ({
-      output: b.output ?? a.output,
-      isAsync: a.isAsync || b.isAsync,
-      isGenerator: a.isGenerator || b.isGenerator,
-      scopedVariables: a.scopedVariables.concat(b.scopedVariables),
-    }),
-    {
-      output: "",
-      isAsync: false,
-      isGenerator: false,
-      scopedVariables: [],
-    }
-  );
+  return {
+    output: typeof options == "string" ? options : options.output,
+    isAsync: parents.some((e) => e.isAsync),
+    isGenerator: parents.some((e) => e.isGenerator),
+    scopedVariables: parents.flatMap((e) => e.scopedVariables),
+  };
 }
 
 function createNodes(...nodes: ohm.Node[]) {
@@ -54,20 +50,43 @@ let actions: StorymaticActionDict<Node> = {
     return createNode(this.sourceString);
   },
   _iter(...children) {
-    let output = children.map((value) => value.js());
-    return createNode(output.map((e) => e.output).join(""), ...output);
+    let js = children.map((node) => node.js());
+    let output = js.map((node) => node.output).join("");
+    return createNode(output, ...js);
   },
 
   AddExp_addition(nodeA, _, nodeB) {
     let [a, b] = createNodes(nodeA, nodeB);
-    return createNode(`${a} + ${b}`, a, b);
+    return createNode(`${a.output} + ${b.output}`, a, b);
   },
   AddExp_subtraction(nodeA, _, nodeB) {
     let [a, b] = createNodes(nodeA, nodeB);
-    return createNode(`${a} + ${b}`, a, b);
+    return createNode(`${a.output} - ${b.output}`, a, b);
+  },
+  decimalNumber(a, b, c, d, e, f, g) {
+    return createNode(this.sourceString);
+  },
+  LiteralExp_parenthesized(_0, node, _1) {
+    let js = node.js();
+    return createNode(`(${js.output})`, js);
+  },
+  NotExp_await(_0, _1, node) {
+    let js = node.js();
+    return createNode({ output: `await ${js.output}`, isAsync: true }, js);
   },
   number(node) {
     return createNode(node.sourceString);
+  },
+  Script(node) {
+    let js = node.js();
+    let output = js.output;
+
+    if (js.scopedVariables.length > 1)
+      output = `let ${js.scopedVariables.join(", ")};\n` + output;
+
+    if (js.isAsync) output += `\n\nexport {};`;
+
+    return createNode(output);
   },
   Statement_expression(node, _) {
     return node.js();
@@ -77,6 +96,7 @@ let actions: StorymaticActionDict<Node> = {
 semantics.addOperation<Node>("js", actions);
 
 type SMNode = Node;
+
 declare global {
   var story: StorymaticGrammar;
   var toAST: (text: string) => {};
@@ -88,6 +108,7 @@ declare global {
 declare module "ohm-js" {
   export interface Node {
     js(): SMNode;
+    asIteration(): Node;
   }
 }
 
