6e90288c53ba9677bf1c16db8462c2e2e1e46c4b
Zachary <zsakowitz@gmail.com>
2022-04-22 17:22:52 -0700

    Add semantics object

--- lang.ts
+++ lang.ts
@@ -1,12 +1,18 @@
 import ohm from "ohm-js";
 import extras from "ohm-js/extras/index.js";
-import story from "./story.ohm-bundle.js";
+import story, {
+  StorymaticGrammar,
+  StorymaticSemantics,
+} from "./story.ohm-bundle.js";
 
 globalThis.story = story;
-globalThis.toAST = (text: string, startRule?: string) =>
-  extras.toAST(story.match(text, startRule));
+globalThis.toAST = (text: string) => extras.toAST(story.match(text));
+
+globalThis.semantics = story.createSemantics();
+semantics.addOperation("javascript", {});
 
 declare global {
-  var story: ohm.Grammar;
-  var toAST: any;
+  var story: StorymaticGrammar;
+  var toAST: (text: string) => {};
+  var semantics: StorymaticSemantics;
 }
