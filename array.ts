8a6f5d7520f5ac26df3ebcfd3cb5fce7a5942c6a
Zachary <zsakowitz@gmail.com>
2022-05-05 16:12:59 -0700

    Add stack

---
+++ array.ts
@@ -0,0 +1,19 @@
+class Stack {
+  #stack: number[] = [];
+
+  get length() {
+    return this.#stack.length;
+  }
+
+  push(value: number) {
+    this.#stack.push(value);
+  }
+
+  pop() {
+    return this.#stack.pop();
+  }
+
+  add() {
+    this.#stack.push(this.#stack.pop()! + this.#stack.pop()!);
+  }
+}
