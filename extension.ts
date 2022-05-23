99e706d0af64ff36fcb3142f79e7313632500bf8
Zachary <zsakowitz@gmail.com>
2022-05-23 08:26:25 -0700

    Add things

--- extension.ts
+++ extension.ts
@@ -1,4 +1,6 @@
-type Extension<State> = (state: State) => void | PromiseLike<void>;
+interface Extension<State> {
+  (state: State): void | PromiseLike<void>;
+}
 
 interface FacetConfig<State, Input, Output = readonly Input[]> {
   combine?(value: readonly Input[]): Output;
@@ -8,20 +10,31 @@ interface FacetConfig<State, Input, Output = readonly Input[]> {
   enables?: Extension<State>;
 }
 
-class Facet<State, Input, Output = readonly Input[]>
-  implements FacetConfig<State, Input, Output>
-{
-  combine: (value: readonly Input[]) => Output;
+class Facet<State, Input, Output = readonly Input[]> {
+  combine: (values: readonly Input[]) => Output;
   compare: (a: Output, b: Output) => boolean;
   compareInput: (a: Input, b: Input) => boolean;
   toExtension: (value: Output) => Extension<State>;
   enables?: Extension<State>;
 
   constructor(config: FacetConfig<State, Input, Output>) {
-    this.combine = config.combine || ((value) => value as Output);
+    this.combine = config.combine || ((values) => values as Output);
     this.compare = config.compare || ((a, b) => a === b);
     this.compareInput = config.compareInput || ((a, b) => a === b);
     this.toExtension = config.toExtension;
     this.enables = config.enables;
   }
+
+  of(value: Input): Extension<State> {
+    return this.toExtension(this.combine([value]));
+  }
+
+  compute(
+    deps: (Facet<any, any> | StateField<any>)[],
+    get: (state: State) => Input
+  ) {
+    return this.toExtension();
+  }
 }
+
+class Field<State, Value> {}
