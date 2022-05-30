fe24e4ab448f18872595fe396b190c918e70a271
zsakowitz <zsakowitz@gmail.com>
2022-05-30 14:33:01 -0700

    Rewrite chrome-home.ts

--- chrome-home.ts
+++ chrome-home.ts
@@ -1,44 +1,68 @@
-function isPWA() {
-  return window.matchMedia('(display-mode: standalone)').matches;
-}
-
-function isMac() {
-  return navigator.platform.includes('Mac');
-}
-
-function getActiveShortcuts(event: KeyboardEvent) {
-  let altOnly = event.altKey && !event.metaKey && !event.ctrlKey;
-  let ctrlOnly = event.ctrlKey && !event.metaKey && !event.altKey;
-  let metaOnly = event.metaKey && !event.ctrlKey && !event.altKey;
-  let cmdOnly = isMac() ? metaOnly : ctrlOnly;
-
-  return { altOnly, cmdOnly, ctrlOnly, metaOnly };
-}
-
-function handler(event: KeyboardEvent) {
-  let { cmdOnly } = getActiveShortcuts(event);
-
-  if (cmdOnly && event.key === 'w') {
-    let currentTab = document.querySelector(
-      '[class*="Editor-module-panel"]:focus-within #editor-tabbar-0 > [class*=focused]'
-    );
-
-    if (currentTab) {
-      (currentTab.children[1] as any).click();
-    }
-
-    event.preventDefault();
+namespace Device {
+  export function isPWA() {
+    return window.matchMedia('(display-mode: standalone)').matches;
   }
+
+  export function isMac() {
+    return navigator.platform.includes('Mac');
+  }
+}
+
+namespace Keyboard {
+  export function Keys({ altKey, ctrlKey, metaKey, shiftKey }: KeyboardEvent) {
+    let altOnly = altKey && !metaKey && !ctrlKey;
+    let ctrlOnly = ctrlKey && !metaKey && !altKey;
+    let metaOnly = metaKey && !ctrlKey && !altKey;
+    let cmdOnly = Device.isMac() ? metaOnly : ctrlOnly;
+
+    return {
+      altOnly,
+      cmdOnly,
+      ctrlOnly,
+      metaOnly,
+      shift: shiftKey,
+    };
+  }
+}
+
+namespace VideoPlayer {
+  export function Initialize() {
+    for (let video of document.querySelectorAll('video')) {
+      video.playbackRate = 2;
+    }
+  }
+}
+
+namespace StackBlitz {
+  export function Handler(event: KeyboardEvent) {
+    let { cmdOnly, ctrlOnly } = Keyboard.Keys(event);
+
+    if (event.key === 'w' && ((Device.isPWA() && cmdOnly) || ctrlOnly)) {
+      let currentTab = document.querySelector(
+        '[class*="Editor-module-panel"]:focus-within #editor-tabbar-0 > [class*=focused]'
+      );
+
+      if (currentTab) {
+        (currentTab.children[1] as any).click();
+      }
+
+      event.preventDefault();
+    }
+  }
+
+  if (StackBlitz.PreviousHandler) {
+    window.removeEventListener('keydown', StackBlitz.PreviousHandler);
+  }
+
+  export function Initialize() {
+    window.addEventListener('keydown', Handler);
+  }
+
+  export let PreviousHandler = Handler;
 }
 
 if (location.host.includes('stackblitz.com')) {
-  if (typeof oldChromeHome !== 'undefined') {
-    window.removeEventListener('keydown', oldChromeHome);
-  }
-
-  window.addEventListener('keydown', handler);
-
-  window.oldChromeHome = handler;
+  StackBlitz.Initialize();
+} else if (document.querySelector('video')) {
+  VideoPlayer.Initialize();
 }
-
-declare var oldChromeHome: typeof handler | undefined;
