3cd1830eea79e45a4f5310ae47b73ad142faeb99
zsakowitz <zsakowitz@gmail.com>
2022-05-29 20:02:32 -0700

    Add typeof.ts

--- once.ts
+++ once.ts
@@ -1,11 +1,11 @@
-export function once<F extends (...args: any) => any>(cb: F): F {
+export function once<F extends (this: any, ...args: any) => any>(cb: F): F {
   let called = false;
 
-  function wrapper(...args: any) {
+  function wrapper(this: any, ...args: any) {
     if (called) return;
     called = true;
 
-    return cb(...args);
+    return cb.call(this, ...args);
   }
 
   return wrapper as F;
---
+++ typeof.ts
@@ -0,0 +1,21 @@
+type TypeOf<T> = T extends null
+  ? 'null'
+  : T extends undefined
+  ? 'undefined'
+  : T extends number | Number
+  ? 'number'
+  : T extends string | String
+  ? 'string'
+  : T extends boolean | Boolean
+  ? 'boolean'
+  : T extends bigint | BigInt
+  ? 'bigint'
+  : T extends symbol | Symbol
+  ? 'symbol'
+  : T extends readonly (infer U)[]
+  ? `array of ${TypeOf<U>}`
+  : T extends (...args: any[]) => any
+  ? 'function'
+  : T extends {}
+  ? 'object'
+  : string;
