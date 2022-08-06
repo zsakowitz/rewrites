08e04d2df5783a6b618d1f7877d5b5c60b906bae
Zachary Sakowitz <zsakowitz@gmail.com>
2022-08-06 01:19:28 +0000

    Add first impl of media engine

---
+++ media.js
@@ -0,0 +1,23 @@
+(async () => {
+  const video = document.createElement("video");
+  document.body.append(video);
+
+  video.src =
+    "https://mdn.github.io/learning-area/html/multimedia-and-embedding/video-and-audio-content/rabbit320.mp4";
+
+  video.loop = true;
+  video.controls = true;
+
+  await video.play();
+
+  function handler(/** @type {MediaSessionActionDetails} */ { action }) {
+    console.log(action);
+  }
+
+  navigator.mediaSession.setActionHandler("pause", handler);
+  navigator.mediaSession.setActionHandler("play", handler);
+  navigator.mediaSession.setActionHandler("previoustrack", handler);
+  navigator.mediaSession.setActionHandler("nexttrack", handler);
+  navigator.mediaSession.setActionHandler("seekbackward", handler);
+  navigator.mediaSession.setActionHandler("seekforward", handler);
+})();
