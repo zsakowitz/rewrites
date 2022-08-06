4c0aeacd579b88541e0fb05e67c170439e8aa7e1
Zachary Sakowitz <zsakowitz@gmail.com>
2022-08-06 11:33:50 +0000

    Hide video

--- media.js
+++ media.js
@@ -1,12 +1,13 @@
 (async () => {
   const video = document.createElement("video");
-  document.body.append(video);
+  document.body.prepend(video);
 
   video.src =
     "https://mdn.github.io/learning-area/html/multimedia-and-embedding/video-and-audio-content/rabbit320.mp4";
 
   video.loop = true;
   video.controls = true;
+  video.style.opacity = 0;
 
   await video.play();
 
