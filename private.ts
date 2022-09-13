ce8e6c7c4a51134010c66be3013049da44cfe6ca
Zachary <zsakowitz@gmail.com>
2022-09-13 13:59:21 -0700

    Add private.ts

---
+++ private.ts
@@ -0,0 +1,27 @@
+const data = new WeakMap<PrivateData<any, any>, WeakMap<any, any>>();
+
+export class PrivateData<K = unknown, V = unknown> {
+  constructor() {
+    data.set(this, new WeakMap());
+  }
+
+  get(key: K): V {
+    return data.get(this)?.get(key);
+  }
+
+  set(key: K, value: V): this {
+    data.get(this)?.set(key, value);
+
+    return this;
+  }
+
+  has(key: K): boolean {
+    return data.get(this)?.has(key) || false;
+  }
+
+  delete(key: K): this {
+    data.get(this)?.delete(key);
+
+    return this;
+  }
+}
