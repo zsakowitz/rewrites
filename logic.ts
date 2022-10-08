13d3a92ef82fd8142cf8bfef392295c3d5ad311c
Zachary <zsakowitz@gmail.com>
2022-10-08 10:10:26 -0700

    Add logic.ts

---
+++ logic.ts
@@ -0,0 +1,17 @@
+declare function assert<T>(_: T): void;
+
+export type Nand<A extends boolean, B extends boolean> = A extends true
+  ? B extends true
+    ? false
+    : true
+  : true;
+
+assert<Nand<true, true>>(false);
+assert<Nand<true, false>>(true);
+assert<Nand<false, true>>(true);
+assert<Nand<false, false>>(true);
+
+export type Not<T extends boolean> = Nand<T, T>;
+
+assert<Not<true>>(false);
+assert<Not<false>>(true);
