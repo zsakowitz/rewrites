3e9bc41599fe45a7dd2d1e1fe359ed6c693eadf5
Zachary <zsakowitz@gmail.com>
2022-10-23 10:19:20 -0700

    Explicit resource management

---
+++ explicit-resource-management.ts
@@ -0,0 +1,138 @@
+export interface Disposable {
+  [Symbol.dispose](): void;
+}
+
+export interface AsyncDisposable {
+  [Symbol.asyncDispose](): void;
+}
+
+const dispose = ((Symbol as any).dispose ??= Symbol("Symbol.dispose"));
+
+const asyncDispose = ((Symbol as any).asyncDispose ??= Symbol(
+  "Symbol.asyncDispose"
+));
+
+let disposables: unknown[] | undefined;
+
+export function usingBlock<T>(fn: () => T): T {
+  let value: T;
+  const oldDisposables = disposables;
+  const mustBeDisposed: unknown[] = (disposables = []);
+
+  try {
+    value = fn();
+  } finally {
+    disposables = oldDisposables;
+    const errors: unknown[] = [];
+    let shouldThrow = false;
+
+    for (const disposable of mustBeDisposed) {
+      try {
+        if (
+          (typeof disposable != "object" && typeof disposable != "function") ||
+          disposable == null
+        ) {
+          throw new TypeError(
+            "A disposable was of an improper type: " + typeof disposable
+          );
+        }
+
+        const disposeFn: unknown = (disposable as any)[dispose];
+
+        if (typeof disposeFn != "function") {
+          throw new TypeError("A disposable is missing an @@dispose method.");
+        }
+
+        disposeFn.call(disposable);
+      } catch (error) {
+        errors.push(error);
+      }
+    }
+
+    if (shouldThrow) {
+      throw new AggregateError(errors);
+    }
+  }
+
+  return value;
+}
+
+export async function asyncUsingBlock<T>(fn: () => T): Promise<Awaited<T>> {
+  let value: Awaited<T>;
+  const oldDisposables = disposables;
+  const mustBeDisposed: unknown[] = (disposables = []);
+
+  try {
+    value = await fn();
+  } finally {
+    disposables = oldDisposables;
+    const errors: unknown[] = [];
+    let shouldThrow = false;
+
+    for (const disposable of mustBeDisposed) {
+      try {
+        if (
+          (typeof disposable != "object" && typeof disposable != "function") ||
+          disposable == null
+        ) {
+          throw new TypeError(
+            "A disposable was of an improper type: " + typeof disposable
+          );
+        }
+
+        const asyncDisposeFn: unknown = (disposable as any)[asyncDispose];
+
+        if (typeof asyncDisposeFn == "function") {
+          asyncDisposeFn.call(disposable);
+        } else {
+          const disposeFn: unknown = (disposable as any)[dispose];
+
+          if (typeof disposeFn != "function") {
+            throw new TypeError(
+              "An async disposable is missing an @@asyncDispose or @@dispose method."
+            );
+          }
+
+          disposeFn.call(disposable);
+        }
+      } catch (error) {
+        errors.push(error);
+      }
+    }
+
+    if (shouldThrow) {
+      throw new AggregateError(errors);
+    }
+  }
+
+  return value;
+}
+
+export function using<T extends Disposable | null | undefined>(
+  disposable: T
+): T {
+  if (!disposables) {
+    throw new Error("Cannot call 'using' outside of a using {} block.");
+  }
+
+  disposables.push(disposable);
+  return disposable;
+}
+
+export function asyncUsing<
+  T extends AsyncDisposable | Disposable | null | undefined
+>(disposable: T): T {
+  if (!disposables) {
+    throw new Error("Cannot call 'usingAsync' outside of a using {} block.");
+  }
+
+  disposables.push(disposable);
+  return disposable;
+}
+
+declare global {
+  interface SymbolConstructor {
+    readonly dispose: unique symbol;
+    readonly asyncDispose: unique symbol;
+  }
+}
