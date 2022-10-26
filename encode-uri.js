19045b82b54d7173f83aad1aac43019b3e524564
Zachary <zsakowitz@gmail.com>
2022-10-26 15:32:53 -0700

    Add file to encode URI into javascript: bookmark

---
+++ encode-uri.js
@@ -0,0 +1,21 @@
+// Encodes standard input into a javascript: bookmark.
+
+console.log("javascript:");
+
+const { stdin } = process;
+
+stdin.setEncoding("utf-8");
+
+stdin.on("readable", () => {
+  let chunk;
+
+  while ((chunk = stdin.read())) {
+    console.log(encodeURI(chunk));
+  }
+});
+
+// 'end' will be triggered once when there is no more data available
+stdin.on("end", () => {
+  console.log(";void 0");
+  process.exit(0);
+});
