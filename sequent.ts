6f3310452685a5ae56562ebcea147441f1ca61ca
Zachary <zsakowitz@gmail.com>
2022-12-08 09:00:20 -0800

    Add sequent.ts

---
+++ sequent.ts
@@ -0,0 +1,20 @@
+// A parser and evaluator for boolean logic and sequent calculus.
+
+export type Expression =
+  | { readonly type: "atomic"; readonly value: string }
+  | { readonly type: "true" }
+  | { readonly type: "false" }
+  | {
+      readonly type: "and"
+      readonly left: Expression
+      readonly right: Expression
+    }
+  | {
+      readonly type: "or"
+      readonly left: Expression
+      readonly right: Expression
+    }
+  | {
+      readonly type: "not"
+      readonly value: Expression
+    }
