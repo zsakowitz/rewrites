98b34a5252d6f9ce45cebf5ea50e5395a0f8b218
Zachary <zsakowitz@gmail.com>
2022-12-24 11:27:39 -1000

    Remove unused declarations

--- env.d.ts
+++ env.d.ts
@@ -1,9 +1,4 @@
 // Declares modules without @types/... declarations.
 
 declare module "es-observable-tests"
-declare module "lamejs"
 declare module "promises-aplus-tests"
-
-declare module "web-audio-api" {
-  export { AudioContext }
-}
