b0580de91ddaf2538841f83cc552abaca93d4671
Zachary <zsakowitz@gmail.com>
2022-12-28 16:00:47 -1000

    Make README auto format code

--- README.js
+++ README.js
@@ -1,45 +1,48 @@
 // The README generator for this repo.
 
-import * as fs from "fs/promises"
+import { spawn } from "child_process"
+import { readdir, readFile, writeFile } from "fs/promises"
 
-let files = await fs.readdir(".")
-files = files.filter((path) => path.endsWith(".js") || path.endsWith(".ts"))
+const files = (await readdir(".")).filter(
+  (path) => path.endsWith(".js") || path.endsWith(".ts")
+)
 
 const bodies = files.map(async (path) => {
-  const contents = await fs.readFile(path, { encoding: "utf8" })
+  const contents = await readFile(path, { encoding: "utf8" })
 
-  let info = []
+  const info = []
   for (const line of contents.split("\n")) {
     if (line.startsWith("//")) {
       info.push(line.slice(2).trim())
     } else break
   }
 
-  let tags = []
-  tags = (info.join(" ").match(/#\w+/g) || []).join(" ")
+  const tags = (info.join(" ").match(/#\w+/g) || []).join(" ")
+  const body = info.join(" ").replace(/#\w+/g, "").replace(/\s+/g, " ").trim()
 
-  let body = info.join(" ").replace(/#\w+/g, "").replace(/\s+/g, " ").trim()
-
-  return `**[${path}](./${path})**${tags ? " " + tags : ""}
-
-${body}`
+  return `**[${path}](./${path})**${tags ? " " + tags : ""}${
+    body ? "\n\n" + body : ""
+  }`
 })
 
 const result = await Promise.all(bodies)
 
-fs.writeFile(
+writeFile(
   "./README.md",
   `# zsakowitz/rewrites
 
 This repository contains lots of test projects that I've created. The name is a
-relic of its initial creation, because I used it to rewrite the Iterator Helpers
-library. Now it contains everything from stacks to language parsers to arithmetic
-in the TS type system. Enjoy the ${
+relic from its initial creation, when I used it to rewrite the Iterator Helpers
+proposal. Now it contains everything from stacks to language parsers to the JS
+standard library implemented solely in the TS type system. Enjoy the ${
     bodies.length
-  } files this repository has to offer.
+  } files
+this repository has to offer.
 
 # File listing
 
 ${result.join("\n\n<br>\n\n")}
 `.trim()
 )
+
+spawn("npx", ["prettier", "--write", "."])
