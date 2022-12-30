752234fef3369f849fc7214117fe8e6bc4d64599
Zachary <zsakowitz@gmail.com>
2022-12-30 15:41:16 -0800

    Fix hooks and download react

--- hooks.ts
+++ hooks.ts
@@ -1,9 +1,9 @@
 // An implementation of React Hooks. #rewrite
 
 let hookId = 0
-let fnCreated = 0
 let isRenderCycleQueued = false
 let renderFn: (() => void) | undefined
+let postRenderEffects: (() => void)[] = []
 
 /** Queues a rerender. */
 export function queueRerender() {
@@ -21,29 +21,25 @@ export function queueRerender() {
 export function render(fn: () => void) {
   isRenderCycleQueued = false
   hookId = 0
-  fnCreated = 0
   renderFn = fn
+  setTimeout(() => postRenderEffects.forEach((fn) => fn()))
   fn()
-  console.debug(`Ran ${hookId} useState and useConst hooks.`)
 }
 
 const consts: Record<number, any> = {}
 
 /** Preserves a constant value across render cycles. */
 export function useConst<T>(value: () => T): T {
-  fnCreated++ // For the function passed to this useConst call.
-
   const id = ++hookId
   return id in consts ? consts[id] : (consts[id] = value())
 }
 
 const states: Record<number, any> = {}
 
-/** Preserves mutable state across render cycles. */
+/** Preserves mutable state across render cycles. Re-renders component after `setState`. */
 export function useState<T>(initial: T) {
   const id = ++hookId
 
-  fnCreated++ // For `update`
   const update = (newValue: T, options?: { queueRerender?: boolean }) => {
     states[id] = newValue
 
@@ -58,11 +54,15 @@ export function useState<T>(initial: T) {
   ]
 }
 
+/** Preserves a value across render cycles. Safe to modify in effects. */
+export function useRef<T>(initial: T): { value: T } {
+  return useConst(() => ({ value: initial }))
+}
+
 /** Returns `true` if this is the first render cycle. */
 export function useIsFirstRenderCycle(): boolean {
   let isFirstRenderCycle = false
 
-  // No fnCreated++ because `useConst` calls it automatically
   useConst(() => {
     isFirstRenderCycle = true
   })
@@ -70,31 +70,47 @@ export function useIsFirstRenderCycle(): boolean {
   return isFirstRenderCycle
 }
 
-/** Runs an effect only when its dependencies change. */
-export function useEffect(fn: () => void, deps: readonly unknown[]) {
-  fnCreated++ // For `fn`
-  const [previousDeps, setPreviousDeps] = useState(deps)
+/** Runs a side effect only when its dependencies change. */
+export function useEffect(
+  fn: () => void | (() => void),
+  deps?: readonly unknown[]
+) {
+  const cleanup = useRef<(() => void) | void>(undefined)
+  const previousDeps = useRef(deps)
 
   if (
     useIsFirstRenderCycle() ||
-    (fnCreated++, // For the .some callback
-    previousDeps.some((dep, depIndex) => dep !== deps[depIndex]))
+    !previousDeps.value ||
+    !deps ||
+    previousDeps.value.some((dep, depIndex) => dep !== deps[depIndex])
   ) {
-    setPreviousDeps(deps, { queueRerender: false })
-    fn()
+    previousDeps.value = deps
+
+    postRenderEffects.push(() => {
+      if (cleanup.value) {
+        cleanup.value()
+        cleanup.value = undefined
+      }
+
+      cleanup.value = fn()
+    })
   }
 }
 
 /** Preserves a computed value as long as none of its dependencies have changed. */
 export function useMemo<T>(fn: () => T, deps: readonly unknown[]): T {
-  let [value, setValue] = useState(null! as T)
+  const memoed = useRef<T>(null!)
+  const previousDeps = useRef(deps)
 
-  // No `fnCreated++` because `useEffect` does it for us
-  useEffect(() => {
-    setValue((value = fn()))
-  }, deps)
+  if (
+    useIsFirstRenderCycle() ||
+    previousDeps.value.some((dep, depIndex) => dep !== deps[depIndex])
+  ) {
+    previousDeps.value = deps
+    memoed.value = fn()
+  }
 
-  return value
+  return memoed.value
 }
 
 /** Preserves an HTML element across render cycles. */
@@ -103,7 +119,6 @@ export function useElement<K extends keyof HTMLElementTagNameMap>(
   props: Record<string, any> = {},
   listeners: Record<string, (event: Event) => void> = {}
 ): HTMLElementTagNameMap[K] {
-  // No `fnCreated++` because `useConst` does it for us
   const node = useConst(() => {
     const node = document.createElement(tagName)
 
@@ -114,12 +129,15 @@ export function useElement<K extends keyof HTMLElementTagNameMap>(
     return node
   })
 
+  const previousProps = useConst<Record<string, any>>(() => ({}))
+
   for (const key in props) {
     const value = props[key]
 
-    useEffect(() => {
+    if (value != previousProps[key]) {
+      previousProps[key] = value
       ;(node as any)[key] = value
-    }, [value])
+    }
   }
 
   return node
