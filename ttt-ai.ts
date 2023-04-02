c8c1115db0ed83e5781fe0128f6eb3e88d321f30
Zachary <zsakowitz@gmail.com>
2023-04-02 16:27:17 -0700

    Add minesweeper.ts

---
+++ ttt-ai.ts
@@ -0,0 +1,100 @@
+// A class-based TicTacToe implementation.
+
+type Player = 1 | 2
+type Square = 0 | Player
+
+class TicTacToe {
+  static randomPosition(): 0 | 1 | 2 {
+    return Math.floor(3 * Math.random()) as 0 | 1 | 2
+  }
+
+  readonly board: [
+    [Square, Square, Square],
+    [Square, Square, Square],
+    [Square, Square, Square]
+  ] = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+  ]
+
+  currentPlayer: Player = 1
+
+  play(row: 0 | 1 | 2, column: 0 | 1 | 2): boolean {
+    if (!this.board[row][column]) {
+      this.board[row][column] = this.currentPlayer
+      this.currentPlayer = (3 - +this.currentPlayer) as 1 | 2
+      return true
+    }
+
+    return false
+  }
+
+  winner(): 0 | Player {
+    if (
+      (this.board[0][0] == 1 &&
+        this.board[0][1] == 1 &&
+        this.board[0][2] == 1) ||
+      (this.board[1][0] == 1 &&
+        this.board[1][1] == 1 &&
+        this.board[1][2] == 1) ||
+      (this.board[2][0] == 1 &&
+        this.board[2][1] == 1 &&
+        this.board[2][2] == 1) ||
+      (this.board[0][0] == 1 &&
+        this.board[1][0] == 1 &&
+        this.board[2][0] == 1) ||
+      (this.board[0][1] == 1 &&
+        this.board[1][1] == 1 &&
+        this.board[2][1] == 1) ||
+      (this.board[0][2] == 1 &&
+        this.board[1][2] == 1 &&
+        this.board[2][2] == 1) ||
+      (this.board[0][0] == 1 &&
+        this.board[1][1] == 1 &&
+        this.board[2][2] == 1) ||
+      (this.board[0][2] == 1 && this.board[1][1] == 1 && this.board[2][0] == 1)
+    ) {
+      return 1
+    }
+
+    if (
+      (this.board[0][0] == 2 &&
+        this.board[0][1] == 2 &&
+        this.board[0][2] == 2) ||
+      (this.board[1][0] == 2 &&
+        this.board[1][1] == 2 &&
+        this.board[1][2] == 2) ||
+      (this.board[2][0] == 2 &&
+        this.board[2][1] == 2 &&
+        this.board[2][2] == 2) ||
+      (this.board[0][0] == 2 &&
+        this.board[1][0] == 2 &&
+        this.board[2][0] == 2) ||
+      (this.board[0][1] == 2 &&
+        this.board[1][1] == 2 &&
+        this.board[2][1] == 2) ||
+      (this.board[0][2] == 2 &&
+        this.board[1][2] == 2 &&
+        this.board[2][2] == 2) ||
+      (this.board[0][0] == 2 &&
+        this.board[1][1] == 2 &&
+        this.board[2][2] == 2) ||
+      (this.board[0][2] == 2 && this.board[1][1] == 2 && this.board[2][0] == 2)
+    ) {
+      return 2
+    }
+
+    return 0
+  }
+
+  playRandomly() {
+    let row
+    let column
+
+    do {
+      row = TicTacToe.randomPosition()
+      column = TicTacToe.randomPosition()
+    } while (!this.play(row, column))
+  }
+}
