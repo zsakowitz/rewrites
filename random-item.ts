57bfdde05df55b8f9fd8e9a981cebdb3fc586f15
Zachary <zsakowitz@gmail.com>
2023-04-14 19:59:31 -0700

    Fix things

--- random-item.ts
+++ random-item.ts
@@ -2,7 +2,14 @@
 
 export function randomItem<T>(values: readonly [T, ...T[]]): T
 export function randomItem<T>(values: readonly T[]): T | undefined
-export function randomItem(values: string): string | undefined
+export function randomItem<T extends string>(
+  values: T,
+): string extends T
+  ? string | undefined
+  : T extends ""
+  ? string | undefined
+  : string
+export function randomItem<T>(values: ArrayLike<T> & { 0: T }): T
 export function randomItem<T>(values: ArrayLike<T>): T | undefined
 export function randomItem<T>(values: ArrayLike<T>): T | undefined {
   return values[Math.floor(Math.random() * values.length)]
