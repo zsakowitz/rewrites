660ebc34dbd7ab267f61dcaed043045d7f079bbb
Zachary <zsakowitz@gmail.com>
2023-05-29 18:18:15 -0700

    Document phonotactics

--- ithkuil/generator/phonotactics/word-initial.ts
+++ ithkuil/generator/phonotactics/word-initial.ts
@@ -1,13 +1,30 @@
 import { isLegalConsonantForm } from "./general"
 
+/**
+ * A regular expression matching legal single-letter word-initial consonant
+ * forms.
+ */
 export const LEGAL_WORD_INITIAL_1_CONSONANT_FORMS = /^[^ļ]$/
 
+/**
+ * A regular expression matching legal two-letter word-initial consonant forms.
+ */
 export const LEGAL_WORD_INITIAL_2_CONSONANT_FORMS =
   /^(?:[pbtdkg][rlřwy]|[pk][sšç]|[bg][zž]|[kg][mn]|[sš][ptkfţxcčř]|[zž][bdgvḑżjmnňrwyl]|[szšž][mnňlrwyřv]|[cżčj][rlmnňwv]|[cč][ptkfţh]|[żj][bdgvḑx]|x[ptcčmnlrw]|ç[ptcčkmnňlrřw]|[fvţḑ][rlmnňwyř]|[fţ][ptkcčç]|[vḑ][bdgżj]|ļ[pktcčçmnňwy]|h[lrmnw]|[mn][lrwyř]|ň[lrw]|[lr][wy]|bv|bḑ|dv|gv|gḑ|kf|kh|kç|kţ|pf|ph|px|pç|pļ|pţ|tf|th|tx|tç|tļ)$/
 
+/**
+ * A regular expression matching legal three-letter word-initial consonant
+ * forms.
+ */
 export const LEGAL_WORD_INITIAL_3_CONSONANT_FORMS =
   /^(?:[pk][sš].|[bd][zž].|[ptk][fţhļ][wy]|[bdg][vḑ][wy]|[tkpbdg][lr][wy]|[ptk]ç[mnň]|[pk][fţ][wy]|[pt]ļ[wy]|[sšç][ptk][wyřlr]|[zž][bdg][wyřlr]|[sšzžç][mnň][wy]|hlw|hrw|hmw|hnw|hmy|hny|hll|hrr|hmm|hnn|[cč][ptk][rlwyř]|[żj][bdg][rlwyř]|[cčżj][mnň][wy]|[fţ]l[wy]|x[pt][lrwy]|x[mn][wy]|x[cč]w)$/
 
+/**
+ * Checks whether a consonant form is allowed in word-initial position.
+ * @param consonantForm The consonant form to check.
+ * @returns A boolean indicating whether the consonant form is a legal
+ * word-initial consonant form.
+ */
 export function isLegalWordInitialConsonantForm(text: string) {
   if (!isLegalConsonantForm(text)) {
     return false
