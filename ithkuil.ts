f9a531ca0fa74b79349fd49ffeda5d224ab0ea55
Zachary Sakowitz <zsakowitz@gmail.com>
2023-06-20 15:49:26 -0400

    Fix things

--- ithkuil.ts
+++ ithkuil.ts
@@ -1,15 +1,51 @@
-import { referentialToIthkuil } from "@zsnout/ithkuil"
+import {
+  ALL_AFFILIATIONS,
+  ALL_CONFIGURATIONS,
+  ALL_ESSENCES,
+  ALL_EXTENSIONS,
+  ALL_PERSPECTIVES,
+  caToIthkuil,
+  geminateCA,
+} from "@zsnout/ithkuil"
 
-const result = referentialToIthkuil({
-  referents: ["1m:BEN"],
-  specification: "CTE",
-  affixes: [
-    {
-      type: 1,
-      degree: 3,
-      cs: "c",
-    },
-  ],
-})
+console.log(`Ca slot              Form     Geminate    Full Ca slot
+-----------------    ----     --------    -----------------`)
 
-console.log(result)
+for (const affiliation of ALL_AFFILIATIONS) {
+  for (const configuration of ALL_CONFIGURATIONS) {
+    for (const essence of ALL_ESSENCES) {
+      for (const extension of ALL_EXTENSIONS) {
+        for (const perspective of ALL_PERSPECTIVES) {
+          const result = caToIthkuil({
+            affiliation,
+            configuration,
+            essence,
+            extension,
+            perspective,
+          })
+
+          console.log(
+            (
+              [
+                affiliation == "CSL" ? "" : affiliation,
+                configuration == "UPX" ? "" : configuration,
+                extension == "DEL" ? "" : extension,
+                perspective == "M" ? "" : perspective,
+                essence == "NRM" ? "" : essence,
+              ]
+                .filter((x) => x)
+                .join("-") || "[default Ca]"
+            ).padEnd(20, " "),
+
+            result.padEnd(8, " "),
+            geminateCA(result).padEnd(12, " "),
+
+            [affiliation, configuration, extension, perspective, essence].join(
+              "-",
+            ),
+          )
+        }
+      }
+    }
+  }
+}
