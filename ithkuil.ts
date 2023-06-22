74df762cdef183c487e3758f9d28d11ff8f7f801
Zachary Sakowitz <zsakowitz@gmail.com>
2023-06-22 14:21:22 -0400

    Update ithkuil.ts

--- ithkuil.ts
+++ ithkuil.ts
@@ -1,51 +1,44 @@
-import {
-  ALL_AFFILIATIONS,
-  ALL_CONFIGURATIONS,
-  ALL_ESSENCES,
-  ALL_EXTENSIONS,
-  ALL_PERSPECTIVES,
-  caToIthkuil,
-  geminateCA,
-} from "@zsnout/ithkuil"
+import { formativeToIthkuil } from "@zsnout/ithkuil"
 
-console.log(`Ca slot              Form     Geminate    Full Ca slot
------------------    ----     --------    -----------------`)
+const result = formativeToIthkuil({
+  root: "l",
+  type: "UNF/C",
+  stem: 0,
+  context: "RPS",
+  slotVAffixes: [
+    {
+      cs: "sm",
+      degree: 7,
+      type: 1,
+    },
+    {
+      cs: "rļ",
+      degree: 3,
+      type: 1,
+    },
+    {
+      cs: "řg",
+      degree: 4,
+      type: 1,
+    },
+  ],
+  ca: {
+    configuration: "MSS",
+    extension: "PRX",
+    perspective: "G",
+  },
+  slotVIIAffixes: [
+    {
+      cs: "rt",
+      degree: 2,
+      type: 1,
+    },
+    {
+      cs: "cč",
+      degree: 7,
+      type: 1,
+    },
+  ],
+})
 
-for (const affiliation of ALL_AFFILIATIONS) {
-  for (const configuration of ALL_CONFIGURATIONS) {
-    for (const essence of ALL_ESSENCES) {
-      for (const extension of ALL_EXTENSIONS) {
-        for (const perspective of ALL_PERSPECTIVES) {
-          const result = caToIthkuil({
-            affiliation,
-            configuration,
-            essence,
-            extension,
-            perspective,
-          })
-
-          console.log(
-            (
-              [
-                affiliation == "CSL" ? "" : affiliation,
-                configuration == "UPX" ? "" : configuration,
-                extension == "DEL" ? "" : extension,
-                perspective == "M" ? "" : perspective,
-                essence == "NRM" ? "" : essence,
-              ]
-                .filter((x) => x)
-                .join("-") || "[default Ca]"
-            ).padEnd(20, " "),
-
-            result.padEnd(8, " "),
-            geminateCA(result).padEnd(12, " "),
-
-            [affiliation, configuration, extension, perspective, essence].join(
-              "-",
-            ),
-          )
-        }
-      }
-    }
-  }
-}
+console.log(result)
