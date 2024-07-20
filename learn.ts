bcd969f6d278b2753e7e50e4c9097d3f03d1f311
Zachary <zsakowitz@gmail.com>
2024-07-19 21:49:18 -0700

    add some things

---
+++ learn.ts
@@ -0,0 +1,68 @@
+import {
+  FSRS,
+  Grades,
+  Rating,
+  State,
+  createEmptyCard,
+  generatorParameters,
+  type Card,
+  type Grade,
+  type RecordLog,
+  type ReviewLog,
+} from "ts-fsrs"
+
+const params = generatorParameters({ enable_fuzz: true })
+
+const f = new FSRS(params)
+
+const card = createEmptyCard(
+  undefined,
+  (c): CardUnchecked => ({ ...c, cid: crypto.randomUUID() }),
+)
+
+interface CardUnchecked extends Omit<Card, "due" | "last_review" | "state"> {
+  cid: string
+  due: Date | number
+  last_review?: Date | number
+  state: State
+}
+
+interface RevLogUnchecked extends Omit<ReviewLog, "due" | "review"> {
+  cid: string
+  due: Date | number
+  state: State
+  review: Date | number
+  rating: Rating
+}
+
+interface RepeatRecordLog {
+  card: CardUnchecked
+  log: RevLogUnchecked
+}
+
+function repeatAfterHandler(recordLog: RecordLog) {
+  const record: { [key in Grade]: RepeatRecordLog } = {} as {
+    [key in Grade]: RepeatRecordLog
+  }
+  for (const grade of Grades) {
+    record[grade] = {
+      card: {
+        ...(recordLog[grade].card as Card & { cid: string }),
+        due: recordLog[grade].card.due.getTime(),
+        state: recordLog[grade].card.state,
+        last_review: recordLog[grade].card.last_review,
+      },
+      log: {
+        ...recordLog[grade].log,
+        cid: (recordLog[grade].card as Card & { cid: string }).cid,
+        due: recordLog[grade].log.due.getTime(),
+        review: recordLog[grade].log.review.getTime(),
+        state: recordLog[grade].log.state,
+        rating: recordLog[grade].log.rating,
+      },
+    }
+  }
+  return record
+}
+
+const log = f.repeat(card, new Date(), repeatAfterHandler)
---
+++ text.tsx
@@ -0,0 +1,48 @@
+import { h, render } from "./easy-jsx.js"
+
+// const el = (
+//   <div style="display:flex;align-items:center;gap:-0.5rem">
+//     {/* <span style="writing-mode:vertical-lr">できます</span>
+//     <div style="display:flex;flex-direction:column;align-items:center;margin: 0 -0.5rem;">
+//       <span>日本語</span>
+//       <span style="font-size:2rem;margin: -0.75rem 0;">が</span>
+//       <span>すこし</span>
+//     </div>
+//     <span style="writing-mode:vertical-lr">ザカリは</span> */}
+//   </div>
+// )
+
+const el = (
+  // <div style="display:grid;grid-template-columns:1fr 1fr 1fr 1fr 1fr;width:fit-content;line-height:1">
+  //   {/* <span>で</span>
+  //   <span>日</span>
+  //   <span>本</span>
+  //   <span>語</span>
+  //   <span>ザ</span>
+
+  //   <span>き</span>
+  //   <span style="grid-column: span 3;grid-row: span 2;font-size:2rem;display:flex;align-items:center;justify-content:center">
+  //     が
+  //   </span>
+  //   <span>カ</span>
+
+  //   <span>ま</span>
+  //   <span>リ</span>
+
+  //   <span>す</span>
+  //   <span>す</span>
+  //   <span>こ</span>
+  //   <span>し</span>
+  //   <span>は</span> */}
+  // </div>
+  <div style="display:flex">
+    <span style="writing-mode:vertical-lr">いま、　</span>
+    <span style="writing-mode:vertical-lr">日本ごが</span>
+    <span style="writing-mode:vertical-lr">すこし</span>
+    <span style="writing-mode:vertical-lr">できます</span>
+    <span style="writing-mode:vertical-lr">すごい</span>
+    <span style="writing-mode:vertical-lr">ですね？</span>
+  </div>
+)
+
+render(document.body, el)
