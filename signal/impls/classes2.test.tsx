925ce9421192ec5356bf7798ca5cfe8824951f93
Zachary <zsakowitz@gmail.com>
2024-08-04 07:11:14 +0000

    it works :spair:

---
+++ signal/impls/classes2.test.tsx
@@ -0,0 +1,62 @@
+/** @jsxRuntime automatic */
+/** @jsxImportSource ./classes2 */
+
+import { memo, signal, untrack } from "./classes"
+import { render, type JSX } from "./classes2/jsx-runtime"
+
+type Truthy<T> = Exclude<T & {}, 0 | "" | false | 0n>
+
+function Show<T, U extends JSX.Element>({
+  when,
+  children,
+}: {
+  when(): T
+  children: Exclude<U, Function> | ((fn: () => Truthy<T>) => U)
+}): () => U | undefined {
+  const getValue = memo(when, undefined, (a, b) => !a === !b)
+
+  return memo(() => {
+    const value = getValue()
+
+    if (value) {
+      return untrack(inner1)
+    }
+  })
+
+  function inner1() {
+    if (typeof children == "function") {
+      return children(inner2)
+    } else {
+      return children
+    }
+  }
+
+  function inner2() {
+    let value
+    if (!untrack(getValue) || !(value = when())) {
+      throw new Error("Invalid access in <Show />")
+    }
+    return value satisfies NonNullable<T> as Truthy<T>
+  }
+}
+
+function Main() {
+  console.log("main")
+  const [shown, setShown] = signal(false)
+
+  return (
+    <div>
+      <h1>world</h1>
+      <button
+        ref={(el) => {
+          el.addEventListener("click", () => setShown(true))
+        }}
+      >
+        magic!
+      </button>
+      <Show when={shown}>look i exist!</Show>
+    </div>
+  )
+}
+
+render(document.body, Main)
---
+++ signal/impls/classes2/jsx-runtime.ts
@@ -0,0 +1,86 @@
+import { immediateEffect, memo, onCleanup, root } from "../classes"
+
+export function render(el: HTMLElement, fn: () => JSX.Element) {
+  return root(() => insert(el, null, fn())).dispose
+}
+
+export function h(
+  tag: string | ((props: {}) => JSX.Element),
+  props: JSX.IntrinsicElements[keyof JSX.IntrinsicElements] | null,
+  ...children: JSX.Element[]
+): JSX.Element {
+  if (typeof tag == "function") {
+    props = props ?? {}
+    if (!("children" in props) && children.length) {
+      if (children.length == 1) {
+        Object.assign(props, { children: children[0] })
+      } else {
+        Object.assign(props, { children })
+      }
+    }
+    return tag(props)
+  }
+
+  const el = document.createElement(tag)
+  insert(el, null, props?.children ?? children)
+  if (props?.ref) {
+    props.ref(el as never)
+  }
+  console.log(el)
+  return el
+}
+
+function insert(parent: Node, before: Node | null, children: JSX.Element) {
+  if (typeof children == "function") {
+    const comment = document.createComment("")
+    parent.insertBefore(comment, before)
+    const m = memo(children)
+    immediateEffect(() => insert(parent, comment, m()))
+    return
+  }
+
+  if (Array.isArray(children)) {
+    for (const grandchild of children) {
+      insert(parent, before, grandchild)
+    }
+    return
+  }
+
+  if (children instanceof Node) {
+    parent.insertBefore(children, before)
+    onCleanup(() => parent.removeChild(children))
+    return
+  }
+
+  if (children) {
+    const text = document.createTextNode(String(children))
+    parent.insertBefore(text, before)
+    onCleanup(() => parent.removeChild(text))
+  }
+}
+
+export namespace JSX {
+  export type Element =
+    | string
+    | number
+    | bigint
+    | boolean
+    | (() => Element)
+    | Element[]
+    | Node
+    | null
+    | undefined
+
+  export type IntrinsicElements = {
+    [K in keyof HTMLElementTagNameMap]: {
+      ref?(el: HTMLElementTagNameMap[K]): void
+      children?: JSX.Element
+    }
+  }
+
+  export interface ElementChildrenAttribute {
+    children: {}
+  }
+}
+
+export { h as jsx, h as jsxs }
