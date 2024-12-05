e31120dd04fa99581a7990b8648d7e8ac57bb232
sakawi <65452710+zsakowitz@users.noreply.github.com>
2024-12-04 21:52:15 -0800

    add 5.js

---
+++ aoc/2024/5.js
@@ -0,0 +1,38 @@
+import "../util.js"
+
+const [a, b] = input(2024, 5).split("\n\n")
+
+const sheets = b.lines().map((x) => x.split(",").num())
+
+const rules = a.lines().map((x) => x.split("|").num())
+
+const afters = {}
+for (const [a, b] of rules) {
+  ;(afters[a] ??= []).push(b)
+}
+
+// part 1:
+sheets
+  .filter((sheet) => {
+    return rules.every(([a, b]) => {
+      const ai = sheet.indexOf(a)
+      const bi = sheet.indexOf(b)
+      if (ai != -1 && bi != -1) return ai < bi
+      else return true
+    })
+  })
+  .sum((x) => x[(x.length - 1) / 2])
+  .check(6949)
+
+sheets
+  .filter((sheet) => {
+    return !rules.every(([a, b]) => {
+      const ai = sheet.indexOf(a)
+      const bi = sheet.indexOf(b)
+      if (ai != -1 && bi != -1) return ai < bi
+      else return true
+    })
+  })
+  .map((x) => x.sort((a, b) => (afters[a].includes(b) ? -1 : 1)))
+  .sum((x) => x[(x.length - 1) / 2])
+  .check(4145)
