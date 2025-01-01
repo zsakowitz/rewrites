0bf3e76b0738fea0a19ff96b37cd417e4eddf8a4
sakawi <65452710+zsakowitz@users.noreply.github.com>
2025-01-01 22:50:08 +0700

    aoc moved to https://github.com/zsakowitz/aoc

--- aoc/2015/25.js
+++
@@ -1,8 +0,0 @@
-import "../util.js"
-
-input(2015, 25)
-  .ints()
-  .do((x) => pt(x[1], x[0]).idxbrbrbr())
-  .do((x) => ri(2, x))
-  .reduce((c) => (c * 252533) % 33554393, 20151125)
-  .check(2650453)
