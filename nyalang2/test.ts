789954a30ab8199317fb87211b99d9cfb80deba3
sakawi <65452710+zsakowitz@users.noreply.github.com>
2025-08-10 12:56:25 -0400

    more tests

--- nyalang2/test.ts
+++ nyalang2/test.ts
@@ -1,11 +1,11 @@
 import { Var } from "./coercion"
 import { Const } from "./const"
+import { Fn } from "./fn"
+import { ident } from "./id"
 import { FnParams, FnParamsTempl, Param, ParamKind } from "./param"
 import { createEnv } from "./std"
-import { T, Ty } from "./ty"
+import { Bool, Int, Num, T, Ty } from "./ty"
 import { Val } from "./val"
-
-const { Bool, Int, Num } = Ty
 
 function createProps() {
   const env = createEnv()
@@ -22,7 +22,7 @@ function createProps() {
 }
 type Props = ReturnType<typeof createProps>
 
-function test(fn: (props: Props) => boolean | undefined) {
+function test(fn: (props: Props) => boolean | void) {
   const props = createProps()
   const ret = fn(props)
   if (ret === false) {
@@ -46,4 +46,36 @@ test(({ ctx, N }) => {
     ctx.root.coerce.can(val.ty, expect, params)
     && params.get(N.value).eqTo(new Const(3, Int), params)
   )
+})
+
+test(({ env, ctx }) => {
+  {
+    const templ = new FnParamsTempl()
+    const U = new Param("U", ParamKind.Const)
+    templ.set(U, Var.Invar, Int)
+
+    const array = new Ty(T.ArrayFixed, { el: Num, size: [new Const(U, Int)] })
+
+    env.root.pushFn(
+      new Fn(
+        ident("+"),
+        templ,
+        [
+          { name: ident("x"), ty: Num },
+          { name: ident("y"), ty: array },
+        ],
+        array,
+        [],
+        (_ctx, args) => args[1]!,
+      ),
+    )
+  }
+
+  const a = ctx.int("23")
+  const b = ctx.target.arrayCons(ctx, [3], Num, [
+    ctx.num("5.7"),
+    ctx.num("43"),
+    ctx.num("2"),
+  ])
+  ctx.call("+", [a, b])
 })
