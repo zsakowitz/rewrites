191b1a9459220eb282c044eb1ba1ec1635754f5f
sakawi <65452710+zsakowitz@users.noreply.github.com>
2025-10-07 08:44:31 -0700

    rect

--- math/game2/rect.ts
+++ math/game2/rect.ts
@@ -27,48 +27,6 @@ class Grid {
     return cell
   }
 
-  cache = new Map<bigint, number>()
-  calcRect(): number {
-    let ret = 0
-    const n = this.n
-    for (let i = 0n; i < this.w * this.h; i++) {
-      if ((1n << i) & n) {
-        this.n = 1n << i
-        ret ^= this.calcInner()
-      }
-    }
-    this.n = n
-    return ret
-  }
-  calcInner(): number {
-    if (this.cache.has(this.n)) {
-      return this.cache.get(this.n)!
-    }
-    const ret: Nimber[] = []
-
-    for (let x2 = 1; x2 < this.w; x2++) {
-      for (let y2 = 1; y2 < this.h; y2++) {
-        if (!this.get(x2, y2)) continue
-
-        for (let x1 = 1; x1 <= x2; x1++) {
-          for (let y1 = 1; y1 <= y2; y1++) {
-            const r = this.getXorRect(x1, y1, x2 + 1, y2 + 1)
-            this.n ^= r
-            ret.push(this.calcRect())
-            this.n ^= r
-          }
-        }
-      }
-    }
-
-    const m = mex(ret)
-    this.cache.set(this.n, m)
-    if (this.cache.size % 1e4 == 0) {
-      console.error(this.cache.size)
-    }
-    return m
-  }
-
   get text() {
     return Array.from({ length: this.h }, (_, j) =>
       Array.from({ length: this.w }, (_, i) =>
@@ -87,13 +45,77 @@ class Grid {
   }
 }
 
-const n = 6
-const I = new Grid(2 ** n + 1, 2 ** n + 1)
-for (let y = 0; y <= n; y++) {
-  for (let x = 0; x <= n; x++) {
-    I.text = ""
-    I.set(2 ** x, 2 ** y, true)
-    const val = I.calcRect()
+class FillGame extends Grid {
+  private cacheFillGame = new Map<bigint, number>()
+  // calcRect(): number {
+  //   let ret = 0
+  //   const n = this.n
+  //   for (let i = 0n; i < this.w * this.h; i++) {
+  //     if ((1n << i) & n) {
+  //       this.n = 1n << i
+  //       ret ^= this.calcInner()
+  //     }
+  //   }
+  //   this.n = n
+  //   return ret
+  // }
+
+  // fill game is to pick a rectangle and flip all coins in it
+  calcFillGame(): number {
+    if (this.cacheFillGame.has(this.n)) {
+      return this.cacheFillGame.get(this.n)!
+    }
+    const ret: Nimber[] = []
+
+    for (let x2 = 1; x2 < this.w; x2++) {
+      for (let y2 = 1; y2 < this.h; y2++) {
+        if (!this.get(x2, y2)) continue
+
+        for (let x1 = 1; x1 <= x2; x1++) {
+          for (let y1 = 1; y1 <= y2; y1++) {
+            const r = this.getXorRect(x1, y1, x2 + 1, y2 + 1)
+            this.n ^= r
+            ret.push(this.calcFillGame())
+            this.n ^= r
+          }
+        }
+      }
+    }
+
+    const m = mex(ret)
+    this.cacheFillGame.set(this.n, m)
+    if (this.cacheFillGame.size % 1e4 == 0) {
+      console.error(this.cacheFillGame.size)
+    }
+    return m
+  }
+
+  getRow(n: number): bigint {
+    let r = 0n
+    for (let i = 0; i < 32; i++) {
+      if (n & (1 << i)) {
+        r += 1n << (1n << BigInt(i))
+      }
+    }
+    return r
+  }
+
+  getCol(n: number): bigint {
+    let r = 0n
+    for (let i = 0; i < 32; i++) {
+      if (n & (1 << i)) {
+        r += 1n << ((1n << BigInt(i)) * BigInt(this.w))
+      }
+    }
+    return r
+  }
+}
+
+const I = new FillGame(9, 9)
+for (let y = 0; y <= 7; y++) {
+  for (let x = 0; x <= 7; x++) {
+    I.n = I.getCol(y) * I.getRow(x)
+    const val = I.calcFillGame()
     console.write(val.toString().padStart(3) + " ")
   }
   console.log()
