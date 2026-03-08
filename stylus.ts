eed044834d4d6a87997e82d8296913ecf986ee02
sakawi <65452710+zsakowitz@users.noreply.github.com>
2026-03-08 15:13:34 -0700

    stylus

--- stylus.ts
+++ stylus.ts
@@ -1,5 +1,7 @@
 const cv = document.createElement("canvas")
+const cv2 = document.createElement("canvas")
 const ctx = cv.getContext("2d")!
+const ctx2 = cv2.getContext("2d")!
 document.body.appendChild(cv)
 cv.style =
     "width:100vw;height:100vh;position:fixed;top:0;left:0;background:#ddd;touch-action:none"
@@ -31,24 +33,27 @@ tiltY              ${ev.tiltY}
 twist              ${ev.twist}
 `.trim()
 
-    cv.width = devicePixelRatio * cv.clientWidth
-    cv.height = devicePixelRatio * cv.clientHeight
-    ctx.scale(devicePixelRatio, devicePixelRatio)
+    if (
+        cv.width != devicePixelRatio * cv.clientWidth
+        || cv.height != devicePixelRatio * cv.clientHeight
+    ) {
+        cv.width = devicePixelRatio * cv.clientWidth
+        cv.height = devicePixelRatio * cv.clientHeight
+        ctx.scale(devicePixelRatio, devicePixelRatio)
+
+        cv2.width = devicePixelRatio * cv.clientWidth
+        cv2.height = devicePixelRatio * cv.clientHeight
+        ctx2.scale(devicePixelRatio, devicePixelRatio)
+    }
+
     ctx.clearRect(0, 0, cv.width, cv.height)
 
-    const w = 64 * (0.25 + Math.sin(Math.abs(ev.tiltX) * (Math.PI / 180)))
-    const h = 64 * (0.25 + Math.sin(Math.abs(ev.tiltY) * (Math.PI / 180)))
+    const w = 64 * Math.cos(ev.altitudeAngle)
+    const h = 16
     ctx.beginPath()
     ctx.fillStyle = "black"
-    ctx.ellipse(ev.offsetX, ev.offsetY, w, h, 0, 0, 2 * Math.PI)
+    ctx.ellipse(ev.offsetX, ev.offsetY, w, h, ev.azimuthAngle, 0, 2 * Math.PI)
     ctx.fill()
-
-    for (const el of ev.getPredictedEvents()) {
-        ctx.beginPath()
-        ctx.fillStyle = "red"
-        ctx.ellipse(el.clientX, el.clientY, 4, 4, 0, 0, 2 * Math.PI)
-        ctx.fill()
-    }
 })
 
 cv.addEventListener("pointerrawupdate", (ev) => {})
