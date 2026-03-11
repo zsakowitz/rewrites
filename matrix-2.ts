a37f19f62fc41cdd3c08541bdc95c17dd899e556
sakawi <65452710+zsakowitz@users.noreply.github.com>
2026-03-10 21:56:19 -0700

    matrix

--- matrix-2.ts
+++ matrix-2.ts
@@ -1,11 +1,35 @@
-import { add, int, isZero, mul, str, type Rat } from "./matrix-rat"
+import { ANSI } from "./ansi"
+import { add, int, inv, isZero, mul, neg, str, type Rat } from "./matrix-rat"
 
 export class Matrix {
+    static from(raw: (number | bigint | Rat)[][]) {
+        const rows = raw.length
+        const cols = Math.max(0, ...raw.map((x) => x.length))
+        const data: Rat[] = []
+
+        for (let r = 0; r < rows; r++) {
+            for (let c = 0; c < cols; c++) {
+                const el = raw[r]![c]!
+                data.push(
+                    el == null ? int(0)
+                    : typeof el == "object" ? el
+                    : int(el),
+                )
+            }
+        }
+
+        return new Matrix(cols, rows, data)
+    }
+
     constructor(
         readonly cols: number,
         readonly rows: number,
         readonly data: Rat[] = Array(cols * rows).fill(int(0)),
     ) {}
+
+    get(row: number, col: number): Rat {
+        return this.data[row * this.cols + col]!
+    }
 
     set(row: number, col: number, value: Rat) {
         this.data[row * this.cols + col] = value
@@ -16,7 +40,7 @@ export class Matrix {
             throw new Error("cannot scale row by zero")
         }
 
-        for (let i = 0; i <= this.cols; i++) {
+        for (let i = 0; i < this.cols; i++) {
             this.data[i + this.cols * row] = mul(
                 this.data[i + this.cols * row]!,
                 by,
@@ -29,11 +53,16 @@ export class Matrix {
             throw new Error("cannot scale a row into itself")
         }
 
-        for (let i = 0; i <= this.cols; i++) {
-            this.data[i + this.cols * into] = add(
-                this.data[i + this.cols * into]!,
-                mul(this.data[i + this.cols * row]!, by),
-            )
+        for (let i = 0; i < this.cols; i++) {
+            this.set(into, i, add(this.get(into, i), mul(this.get(row, i), by)))
+        }
+    }
+
+    swap(a: number, b: number) {
+        for (let i = 0; i < this.cols; i++) {
+            const c = this.get(a, i)
+            this.set(a, i, this.get(b, i))
+            this.set(b, i, c)
         }
     }
 
@@ -52,11 +81,53 @@ export class Matrix {
             .reduce(
                 (arr, el, i) =>
                     arr
+                    + (el.trim() == "0" ? ANSI.dim
+                    : el.trim()[0] == "-" ? ANSI.red
+                    : ANSI.green)
                     + el.padEnd(len[i % this.cols]!)
-                    + (i % this.cols == this.cols - 1 ? "\n" : "  "),
+                    + ANSI.reset
+                    + (i % this.cols == this.cols - 1 ? "\n" : " "),
                 "",
             )
             .slice(0, -1)
+    }
+
+    clz() {
+        const ret = []
+
+        for (let i = 0; i < this.rows; i++) {
+            let j = 0
+            for (; j < this.cols; j++) {
+                if (this.data[i * this.cols + j]!.n != 0n) {
+                    break
+                }
+            }
+            ret.push(j)
+        }
+
+        return ret
+    }
+
+    rref() {
+        let target = 0
+
+        for (let c = 0; c < this.cols; c++) {
+            const clz = this.clz()
+            let r = clz.findIndex((x) => x == c)
+            if (r == -1) continue
+
+            this.scale(r, inv(this.get(r, c)))
+            this.swap(r, (r = target++))
+
+            for (let a = 0; a < this.rows; a++) {
+                if (a == r) continue
+
+                const head = this.get(a, c)
+                if (head.n == 0n) continue
+
+                this.scaleInto(r, neg(head), a)
+            }
+        }
     }
 }
 
@@ -95,6 +166,22 @@ function grid(cols: number, rows: number): Matrix {
     return m
 }
 
-const m = grid(3, 2)
+function random(cols: number, rows: number): Matrix {
+    const m = new Matrix(cols, rows)
+    for (let i = 0; i < cols * rows; i++) {
+        m.data[i] = int(Math.floor(2 * Math.random()))
+    }
+    return m
+}
 
+const m = Matrix.from([
+    [1, 1, 1, 2, 0],
+    [0, 0, 1, 1, 0],
+    [0, 0, 0, 1, 0],
+])
+
+console.log(m.toString())
+
+m.rref()
+console.log()
 console.log(m.toString())
