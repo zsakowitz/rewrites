0d8f8fd9258c8e1e69379b286fe34ea3c0b783c3
sakawi <65452710+zsakowitz@users.noreply.github.com>
2026-03-11 20:57:20 -0700

    meow

--- cv/index.ts
+++ cv/index.ts
@@ -1,32 +1,17 @@
 import { Canvas, type EventsCanvas } from "./canvas"
-import {
-    ColorPurple,
-    OpacityPointHalo,
-    SizePoint,
-    SizePointHaloWide,
-} from "./dcg"
 import { di } from "./debug"
+import { render, type Object } from "./object"
 import { PathRecorder, type EventsPathRecorder } from "./path-recorder"
-import { getPath, simplifyPath } from "./path-render"
-import {
-    apply,
-    applyList,
-    compose,
-    type PointList,
-    type Transform,
-} from "./transform"
+import { simplifyPath } from "./path-render"
 import { TransformTarget, type EventsScreen } from "./transform-target"
-
-interface CompletePath {
-    points: PointList
-    tx: Transform
-    lw: number
-}
 
 const events: EventsPathRecorder & EventsScreen & EventsCanvas = {
     onCanvasResize() {},
-    onPathFinish(path) {
-        complete(path.points)
+    onPathFinish(raw) {
+        const path = simplifyPath(raw.points)
+        const tx = screen.toLocal()
+        const lw = screen.toLocalDelta(2)
+        objects.push({ type: "path", tx, lw, path })
     },
     onPathUpdate: write,
     onScreenUpdate: write,
@@ -36,29 +21,40 @@ const cv = new Canvas(events)
 const paths = new PathRecorder(events)
 const screen = new TransformTarget(events, cv.el)
 
-const completedPaths: CompletePath[] = []
+const objects: Object[] = []
 
-cv.el.addEventListener("pointermove", paths, { passive: true })
-cv.el.addEventListener("pointerup", paths, { passive: true })
-cv.el.addEventListener("pointercancel", paths, { passive: true })
-cv.el.addEventListener(
-    "pointerdown",
-    (ev) => {
-        if (ev.pointerType != "touch") {
+function getIncomplete(): Object[] {
+    return paths
+        .getIncomplete()
+        .map((x) => ({ type: "pathIncomplete", path: x.points }))
+}
+
+function handleDOMEvent(ev: PointerEvent | WheelEvent) {
+    if (ev.type == "wheel") {
+        screen.handleEvent(ev)
+        return
+    }
+
+    ev = ev as PointerEvent
+
+    cv.el.setPointerCapture(ev.pointerId)
+
+    if (ev.type == "pointerdown") {
+        if (ev.pointerType == "touch") {
+            screen.handleEvent(ev)
+        } else {
             paths.handleEvent(ev)
         }
-    },
-    { passive: true },
-)
-
-function complete(raw: PointList) {
-    const points = simplifyPath(raw)
-    const tx = screen.toLocal()
-    const lw = screen.toLocalDelta(2)
-
-    const path: CompletePath = { points, tx, lw }
-    completedPaths.push(path)
+    } else {
+        paths.handleEvent(ev) || screen.handleEvent(ev)
+    }
 }
+
+cv.el.addEventListener("pointermove", handleDOMEvent, { passive: true })
+cv.el.addEventListener("pointerup", handleDOMEvent, { passive: true })
+cv.el.addEventListener("pointercancel", handleDOMEvent, { passive: true })
+cv.el.addEventListener("pointerdown", handleDOMEvent, { passive: true })
+cv.el.addEventListener("wheel", handleDOMEvent, { passive: true })
 
 function write() {
     di.write`
@@ -69,50 +65,7 @@ ${screen.pos.zy}
     `
 
     cv.clear()
-    writePaths()
-    writePoints()
-}
 
-function writePaths() {
-    const { ctx } = cv
-
-    ctx.strokeStyle = "white"
-    ctx.lineCap = "round"
-    ctx.lineJoin = "round"
-    ctx.fillStyle = "white"
-
-    for (const el of completedPaths) {
-        ctx.lineWidth = screen.toScreenDelta(el.lw)
-        const tx = compose(el.tx, screen.toScreen())
-        ctx.stroke(getPath(applyList(tx, el.points)))
-    }
-
-    ctx.lineWidth = 2
-    for (const path of paths.getIncomplete()) {
-        ctx.stroke(getPath(simplifyPath(path.points)))
-    }
-}
-
-function writePoints() {
-    const { ctx } = cv
-    const tx = screen.toScreen()
-
-    ctx.fillStyle = ColorPurple
-
-    ctx.beginPath()
-    ctx.globalAlpha = OpacityPointHalo
-    ctx.ellipse(
-        ...apply(tx, [3, 4]),
-        SizePointHaloWide,
-        SizePointHaloWide,
-        0,
-        0,
-        2 * Math.PI,
-    )
-    ctx.fill()
-
-    ctx.beginPath()
-    ctx.globalAlpha = 1
-    ctx.ellipse(...apply(tx, [3, 4]), SizePoint, SizePoint, 0, 0, 2 * Math.PI)
-    ctx.fill()
+    render(cv, screen, objects)
+    render(cv, screen, getIncomplete())
 }
