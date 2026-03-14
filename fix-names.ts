5565c8c7e0e24cb865e84f7fd59f67b942278ca5
sakawi <65452710+zsakowitz@users.noreply.github.com>
2026-03-14 11:23:57 -0700

    meow

--- fix-names.ts
+++ fix-names.ts
@@ -1,7 +1,7 @@
 import { rename, utimes } from "node:fs/promises"
 
-await Promise.all(
-    process.argv.slice(2).map(async (rawName, i) => {
+const iter1 = await Promise.all(
+    process.argv.slice(2).map(async (rawName) => {
         const file = Bun.file(rawName)
         const stat = await file.stat()
         let [, dir, , ext] = file.name!.match(
@@ -15,8 +15,21 @@ await Promise.all(
         //             ?.split("/")[0]!
         //             .trim()
         // }
-        const nextName = `${dir}photo-${(i + "").padStart(5, "0")}${ext}`
+        const nextName = `${dir}photo-${crypto.randomUUID()}${ext}`
         await rename(file.name!, nextName)
         await utimes(nextName, stat.birthtime, stat.birthtime)
+        return { dir, ext, path: nextName, time: stat.birthtime }
     }),
 )
+
+iter1.sort((a, b) => +a.time - +b.time)
+
+const iter2 = await Promise.all(
+    iter1.map(async ({ dir, ext, path, time }, i) => {
+        const name = `${dir}photo-${("" + i).padStart(5, "0")}${ext}`
+        await rename(path, name)
+        await utimes(name, time, time)
+    }),
+)
+
+console.log(iter2.length, "files updated")
