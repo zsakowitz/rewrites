b0c792abeb804e6dd868a31df598efdf7932bdff
sakawi <65452710+zsakowitz@users.noreply.github.com>
2026-03-14 18:28:58 -0700

    touch events

--- cv2/2d/movable.ts
+++ cv2/2d/movable.ts
@@ -1,23 +1,34 @@
 import type { Tform2 } from "./tform"
 
+interface Pointer {
+    down: boolean
+    moved: boolean
+
+    // Original coordinates
+    ox: number
+    oy: number
+
+    // Last known coordinates
+    x: number
+    y: number
+}
+
 export class Movable {
     #ow = 0 // offset width of reference element
     #oh = 0 // offset height of reference element
-
-    #touches = new Map<number, Touch>()
 
     /**
      * Transformation from unit space to local space, not including the effects
      * of currently active touch pointers.
      */
-    #ul: Tform2
+    #ul0: Tform2
 
     /**
      * Transformation from unit space to local space, including the effects of
      * currently active touch pointers.
      */
     get ul() {
-        return this.#ul
+        return this.#ul0
     }
 
     /** @param tf Transformation from unit space to local space. */
@@ -27,7 +38,7 @@ export class Movable {
             this.#oh = e!.contentRect.height
         }).observe(el, { box: "device-pixel-content-box" })
 
-        this.#ul = tf
+        this.#ul0 = tf
     }
 
     /** Converts from local space to offset space. */
@@ -68,9 +79,9 @@ export class Movable {
 
             ev.preventDefault()
             if (ev.ctrlKey || ev.metaKey) {
-                this.#onwheel_zoom(ev as WheelEvent)
+                this.#onwheelZoom(ev as WheelEvent)
             } else {
-                this.#onwheel_move(ev as WheelEvent)
+                this.#onwheelMove(ev as WheelEvent)
             }
             return true
         }
@@ -78,8 +89,8 @@ export class Movable {
         return false
     }
 
-    #onwheel_zoom(ev: WheelEvent) {
-        const { sx, sy, tx, ty } = this.#ul
+    #onwheelZoom(ev: WheelEvent) {
+        const { sx, sy, tx, ty } = this.#ul0
 
         const ds =
             ev.deltaMode == 2 ? 2 ** ev.deltaY
@@ -90,7 +101,7 @@ export class Movable {
         const px = (2 * ev.offsetX - this.#ow) / this.#oh
         const py = 1 - (2 * ev.offsetY) / this.#oh
 
-        this.#ul = {
+        this.#ul0 = {
             sx: sx * ds,
             sy: sy * ds,
             tx: tx + px * sx * (1 - ds),
@@ -98,7 +109,7 @@ export class Movable {
         }
     }
 
-    #onwheel_move(ev: WheelEvent) {
+    #onwheelMove(ev: WheelEvent) {
         const wx =
             (ev.deltaMode == 2 ? this.#ow
             : ev.deltaMode == 1 ? 16
@@ -109,15 +120,87 @@ export class Movable {
             : ev.deltaMode == 1 ? 16
             : 1) * ev.deltaY
 
-        const tf = this.#ul
+        const tf = this.#ul0
         const dx = (wx / this.#oh) * 3 * tf.sx
         const dy = -(wy / this.#oh) * 3 * tf.sy
 
-        this.#ul = {
+        this.#ul0 = {
             sx: tf.sx,
             sy: tf.sy,
             tx: tf.tx + dx,
             ty: tf.ty + dy,
         }
+    }
+
+    #pointers = new Map<number, Pointer>()
+
+    #onpointerdown(ev: PointerEvent): boolean {
+        const [a, b] = this.#pointers.values()
+
+        // We can only handle two pointers, so ignore any more.
+        if (b) return true
+
+        // If one finger moved significantly, ignore any further touches.
+        if (a?.moved) return true
+
+        // If a pointer is put down after being pulled up, ignore it.
+        if (this.#pointers.has(ev.pointerId)) return true
+
+        this.#pointers.set(ev.pointerId, {
+            down: true,
+            moved: false,
+            ox: ev.offsetX,
+            oy: ev.offsetY,
+            x: ev.offsetX,
+            y: ev.offsetY,
+        })
+
+        return true
+    }
+
+    #onpointermove(ev: PointerEvent): boolean {
+        const ptr = this.#pointers.get(ev.pointerId)
+        if (!ptr) return false
+
+        // If we released this pointer, ignore any further movement.
+        if (!ptr.down) return true
+
+        ptr.x = ev.offsetX
+        ptr.y = ev.offsetY
+
+        if (!ptr.moved && Math.hypot(ptr.x - ptr.ox, ptr.y - ptr.oy) > 8) {
+            ptr.moved = true
+        }
+
+        return true
+    }
+
+    #onpointerfinish(ev: PointerEvent): boolean {
+        const ptr = this.#pointers.get(ev.pointerId)
+        if (!ptr) return false
+
+        // If any pointer is cancelled, stop tracking the rest.
+        if (ev.type == "pointercancel") {
+            this.#pointers.clear()
+            return true
+        }
+
+        ptr.x = ev.offsetX
+        ptr.y = ev.offsetY
+        ptr.down = false
+
+        let areAllReleased = true
+        for (const v of this.#pointers.values()) {
+            if (v.down) {
+                areAllReleased = false
+                break
+            }
+        }
+        if (areAllReleased) {
+            this.#ul0 = this.ul // Set the new permanent position.
+            this.#pointers.clear()
+        }
+
+        return true
     }
 }
