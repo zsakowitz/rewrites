3d587ab20322c2e31fff94221e2e8fddd6e6d08f
sakawi <65452710+zsakowitz@users.noreply.github.com>
2026-03-17 11:51:23 -0700

    meow

--- cv2/2d/object.ts
+++ cv2/2d/object.ts
@@ -1,13 +1,76 @@
 import type { Canvas2 } from "./canvas"
+import { apply2 } from "./tform"
 
 export interface Object2 {
     visible: boolean
     draw(cv: Canvas2): void
 }
 
-export function objectByDrawFn(draw: (cv: Canvas2) => void): Object2 {
-    return {
-        visible: true,
-        draw,
+export class ByDrawFn implements Object2 {
+    constructor(readonly draw: (cv: Canvas2) => void) {}
+
+    visible = true
+}
+
+export class XorPattern implements Object2 {
+    #bitmap
+
+    constructor(
+        l = 0.7,
+        c = 1,
+        readonly x = 0,
+        readonly y = 0,
+    ) {
+        const cv = new OffscreenCanvas(256, 256)
+        const ctx = cv.getContext("2d")!
+
+        for (let i = 0; i < 256; i++) {
+            for (let j = 0; j < 256; j++) {
+                ctx.fillStyle = `oklch(${l} ${c} ${(360 / 256) * (i ^ j)})`
+                ctx.fillRect(i, j, 1, 1)
+            }
+        }
+
+        this.#bitmap = cv.transferToImageBitmap()
+    }
+
+    visible = true
+
+    draw(cv: Canvas2): void {
+        const [x, y] = apply2(cv.tlo, [this.x, this.y])
+        cv.ctx.imageSmoothingEnabled = false
+        cv.ctx.drawImage(this.#bitmap, x, y, cv.tlo.sx, -cv.tlo.sy)
+    }
+}
+
+export class Oklch implements Object2 {
+    #bitmap
+
+    constructor(
+        c: number,
+        readonly x = 0,
+        readonly y = 0,
+    ) {
+        const cv = new OffscreenCanvas(256, 256)
+        const ctx = cv.getContext("2d")!
+
+        for (let i = 0; i < 256; i++) {
+            for (let j = 0; j < 256; j++) {
+                const l = j / 256
+                const h = i * (360 / 256)
+                ctx.fillStyle = `oklch(${l} ${c} ${h})`
+                ctx.fillRect(i, j, 1, 1)
+            }
+        }
+
+        this.#bitmap = cv.transferToImageBitmap()
+    }
+
+    visible = true
+
+    draw(cv: Canvas2): void {
+        const [x, y] = apply2(cv.tlo, [this.x, this.y])
+        cv.ctx.imageSmoothingEnabled = false
+        cv.ctx.drawImage(this.#bitmap, x, y, cv.tlo.sx, -cv.tlo.sy)
     }
 }
--- cv2/index.ts
+++ cv2/index.ts
@@ -1,34 +1,15 @@
 import { Canvas2 } from "./2d/canvas"
-import { apply2 } from "./2d/tform"
+import { Oklch, XorPattern } from "./2d/object"
 
-const cv = new Canvas2({ sx: 16, sy: 16, tx: 0, ty: 0 })
+const cv = new Canvas2({ sx: 1, sy: 1, tx: 0, ty: 0 })
 
-const { ctx } = cv
 document.body.appendChild(cv.el)
 cv.el.style =
     "position:fixed;inset:0;width:100vw;height:100vh;touch-action:none"
 
-cv.pushf(({ tlo }) => {
-    for (let i = 0; i < 16; i++) {
-        for (let j = 0; j < 16; j++) {
-            ctx.fillStyle = rgb(i / 16, j / 16, 0)
-            const [x, y] = apply2(tlo, [i, j])
-            ctx.fillRect(x, y, tlo.sx, tlo.sy)
-        }
-    }
-})
-
-function rgb(r: number, g: number, b: number) {
-    return (
-        "#"
-        + Math.round(r * 255)
-            .toString(16)
-            .padStart(2, "0")
-        + Math.round(g * 255)
-            .toString(16)
-            .padStart(2, "0")
-        + Math.round(b * 255)
-            .toString(16)
-            .padStart(2, "0")
-    )
-}
+cv.push(new XorPattern(1, 0.3, 0, -1))
+cv.push(new XorPattern(0.2, 0.7, 0, 0))
+cv.push(new XorPattern(1, 0.7, -1, -1))
+cv.push(new XorPattern(0.2, 0.3, -1, 0))
+cv.push(new Oklch(0.2, 0, 1))
+cv.push(new Oklch(0.3, -1, 1))
