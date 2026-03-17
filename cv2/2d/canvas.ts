77d5bbbbd1be312014f19e9cedeec6a21d771b15
sakawi <65452710+zsakowitz@users.noreply.github.com>
2026-03-16 21:25:09 -0700

    meow

--- cv2/2d/canvas.ts
+++ cv2/2d/canvas.ts
@@ -1,3 +1,5 @@
+import type { Tform2 } from "./tform"
+
 export interface CanvasArgs {
     /**
      * Called when the canvas resizes, has its context restored, or becomes
@@ -6,34 +8,66 @@ export interface CanvasArgs {
     onCanvasUpdate(): void
 }
 
-export class Canvas2 {
-    readonly ctx
+interface TouchPointer {
+    down: boolean
+    moved: boolean
 
-    constructor(
-        ev: CanvasArgs,
-        readonly el = document.createElement("canvas"),
-    ) {
+    // first known pointer location, in unit space
+    readonly ox: number
+    readonly oy: number
+
+    // last known pointer location, in unit space
+    x: number
+    y: number
+}
+
+function adjust1(
+    { sx, sy, tx, ty }: Tform2,
+    { ox, oy, x, y }: TouchPointer,
+): Tform2 {
+    return {
+        sx,
+        sy,
+        tx: (x - ox) * sx + tx,
+        ty: (y - oy) * sy + ty,
+    }
+}
+
+export class Canvas2 {
+    readonly el = document.createElement("canvas")
+    readonly ctx = this.el.getContext("2d")!
+
+    #ow = 0
+    #oh = 0
+    #ev
+
+    constructor(ev: CanvasArgs) {
+        this.#ev = ev
+
+        const { el, ctx } = this
+
+        if (!ctx) {
+            throw new Error("Failed to initialize canvas.")
+        }
+
         new ResizeObserver(([e]) => {
-            el.width = e!.contentRect.width * devicePixelRatio
-            el.height = e!.contentRect.height * devicePixelRatio
-            this.reset()
-            ev.onCanvasUpdate()
+            el.width = (this.#ow = e!.contentRect.width) * devicePixelRatio
+            el.height = (this.#oh = e!.contentRect.height) * devicePixelRatio
+            this.#redraw()
         }).observe(el)
 
         el.addEventListener("contextrestored", () => {
-            this.reset()
-            ev.onCanvasUpdate()
+            this.#redraw()
         })
-
-        this.ctx = el.getContext("2d")!
-
-        if (!this.ctx) {
-            throw new Error("Failed to initialize canvas.")
-        }
     }
 
     reset() {
         this.ctx.reset()
         this.ctx.scale(devicePixelRatio, devicePixelRatio)
+    }
+
+    #redraw() {
+        this.reset()
+        this.#ev.onCanvasUpdate()
     }
 }
