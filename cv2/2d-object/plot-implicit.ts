6a5a40fe6650175b4c67d94702cc3c6dfa5aec10
sakawi <65452710+zsakowitz@users.noreply.github.com>
2026-03-18 16:23:07 -0700

    start on plot-implicit

---
+++ cv2/2d-object/plot-implicit.ts
@@ -0,0 +1,128 @@
+import type { Canvas2 } from "../2d/canvas"
+import { Object2 } from "../2d/object"
+import { apply2x, apply2y } from "../2d/tform"
+
+export class PlotImplicit extends Object2 {
+    #f
+
+    constructor(f: (x: number, y: number) => number) {
+        super()
+        this.#f = f
+    }
+
+    draw({ ctx, width, height, tol }: Canvas2): void {
+        const f = this.#f
+        const dox = 32
+        const doy = 32
+
+        ctx.strokeStyle = "black"
+        ctx.lineWidth = 2.5
+        ctx.lineCap = "round"
+        ctx.lineJoin = "round"
+        ctx.beginPath()
+
+        ctx.beginPath()
+        for (let ox = 0; ox < width; ox += dox) {
+            for (let oy = 0; oy < height; oy += doy) {
+                draw(ctx, valueAt, ox, ox + dox, oy, oy + doy)
+            }
+        }
+        ctx.stroke()
+
+        function valueAt(ox: number, oy: number): number {
+            return f(apply2x(tol, ox), apply2y(tol, oy))
+        }
+    }
+}
+
+function draw(
+    ctx: CanvasRenderingContext2D,
+    valueAt: (ox: number, oy: number) => number,
+    xl: number,
+    xr: number,
+    yt: number,
+    yb: number,
+) {
+    const xm = (xl + xr) / 2
+    const ym = (yt + yb) / 2
+
+    const ltRaw = valueAt(xl, yt)
+
+    const lt = ltRaw * (ltRaw > 0 ? 1 : -1)
+    const lb = valueAt(xl, yb) * (ltRaw > 0 ? 1 : -1)
+    const rt = valueAt(xr, yt) * (ltRaw > 0 ? 1 : -1)
+    const rb = valueAt(xr, yb) * (ltRaw > 0 ? 1 : -1)
+
+    const LB = lb > 0
+    const RT = rt > 0
+    const RB = rb > 0
+
+    if (LB && RT && RB && xr - xl < 16) {
+        return
+    }
+
+    if (xr - xl > 8) {
+        draw(ctx, valueAt, xl, xm, yt, ym)
+        draw(ctx, valueAt, xl, xm, ym, yb)
+        draw(ctx, valueAt, xm, xr, yt, ym)
+        draw(ctx, valueAt, xm, xr, ym, yb)
+        return
+    }
+
+    if (LB) {
+        if (RT) {
+            if (RB) {
+                // + +
+                // + +
+            } else {
+                // + +
+                // + -
+                ctx.moveTo(xm, yb)
+                ctx.lineTo(xr, ym)
+            }
+        } else {
+            if (RB) {
+                // + -
+                // + +
+                ctx.moveTo(xm, yt)
+                ctx.lineTo(xr, ym)
+            } else {
+                // + -
+                // + -
+                ctx.moveTo(map(lt / (lt - rt), xr, xl), yt)
+                ctx.lineTo(map(lb / (lb - rb), xr, xl), yb)
+            }
+        }
+    } else {
+        if (RT) {
+            if (RB) {
+                // + +
+                // - +
+                ctx.moveTo(xl, ym)
+                ctx.lineTo(xm, yb)
+            } else {
+                // + +
+                // - -
+                ctx.moveTo(xl, map(lt / (lt - lb), yb, yt))
+                ctx.lineTo(xr, map(rt / (rt - rb), yb, yt))
+            }
+        } else {
+            if (RB) {
+                // + -
+                // - +
+                // ctx.moveTo()
+                // ctx.lineTo()
+                // TODO: saddle point
+            } else {
+                // + -
+                // - -
+                ctx.moveTo(xl, ym)
+                ctx.lineTo(xm, yt)
+            }
+        }
+    }
+}
+
+function map(t: number, v0: number, v1: number) {
+    return t * (v0 - v1) + v1
+}
--- cv2/index.ts
+++ cv2/index.ts
@@ -1,12 +1,12 @@
 import { Grid } from "./2d-object/grid"
 import { XorPattern } from "./2d-object/misc"
-import { PlotByX } from "./2d-object/plot-by-x"
+import { PlotImplicit } from "./2d-object/plot-implicit"
 import { Canvas2 } from "./2d/canvas"
 
-const cv = new Canvas2({ sx: 80.398236, sy: 80.398236, tx: 20, ty: 0 })
+const cv = new Canvas2({ sx: 10, sy: 10, tx: 0, ty: 0 })
 
 document.body.appendChild(cv.el)
 
 cv.push(new Grid())
 cv.push(new XorPattern())
-cv.push(new PlotByX((x) => -Math.tan(x)))
+cv.push(new PlotImplicit((x, y) => Math.sin(x * y) - Math.cos(y)))
