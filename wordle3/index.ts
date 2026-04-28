da6f605406a9c39ea355d24d2b652820b26ce30e
sakawi <65452710+zsakowitz@users.noreply.github.com>
2026-04-27 20:14:45 -0700

    data

--- wordle3/index.ts
+++ wordle3/index.ts
@@ -1,12 +1,13 @@
-import { shuffle } from "./shuffle"
-import { solutions } from "./wordle3-data"
+import { init } from "../parsers/parser-3"
+import { shuffle } from "../shuffle"
+import { solutions } from "./data"
 import {
     check,
     WORD_LENGTH,
     wordFromString,
     wordToString,
     type Word,
-} from "./wordle3-score"
+} from "./score"
 
 function pick(words: Set, max: number): Set {
     return shuffle(words).slice(0, max)
@@ -27,57 +28,35 @@ const SOLUTIONS: Set = shuffle(solutions).map(wordFromString)
 
 const PARTITIONS = Array<number>(4 ** WORD_LENGTH).fill(0)
 
+function variance(x: number[]): number {
+    const avg = x.reduce((a, b) => a + b, 0) / x.length
+    return x.reduce((a, b) => a + (b - avg) * (b - avg), 0) / x.length
+}
+
 // Let `P` be a set and `G` be a word. Partition `P` by the score when each word
-// is checked against the guess `G`. Then the "partition size" is defined to be
-// the size of the largest partition.
-//
-// Intuitively, the partition size answers, "how many words COULD be left in the
-// worst-case scenario after a given guess?"
-//
-// For instance, if some guess gives a score of `00100` for 45 words, a score of
-// `21000` for 23 words, and no other scores, then this function returns `45`.
-//
-// Takes `O(|possible|)` time.
-function maxPartitionSize(possible: Set, guess: Word): number {
+// is checked against the guess `G`. This function returns the variance
+// of the sizes of the partitions.
+function varPartitionSize(possible: Set, guess: Word): number {
     PARTITIONS.fill(0)
-    let maxSize = 0
 
     for (let i = 0; i < possible.length; i++) {
         const solution = possible[i]!
         const score = check(solution, guess)
         PARTITIONS[score] ??= 0
-        const partitionSize = ++PARTITIONS[score]!
-        if (partitionSize > maxSize) maxSize = partitionSize
+        PARTITIONS[score]!++
     }
 
-    return maxSize
-}
-
-// Strategy which minimizes the resulting partition size.
-//
-// Takes `O(|possible|²)` time.
-function stratMinimizePartitionSize(possible: Set): Word {
-    let min = maxPartitionSize(possible, possible[0]!)
-    let ret = possible[0]!
-
-    for (let i = 1; i < possible.length; i++) {
-        const guess = possible[i]!
-        const size = maxPartitionSize(possible, guess)
-        if (size < min) {
-            min = size
-            ret = guess
-        }
-    }
-
-    return ret
+    return variance(PARTITIONS.filter((x) => x != 0))
 }
 
 // Statistic which counts how many guesses it takes to get every word. If a
 // strategy takes more than `max` guesses, it is replaced with `Infinity`.
-function statGuesses(possible: Set, strat: Strategy, max = 6): number[] {
-    const ret: number[] = []
+function statGuesses(possible: Set, strat: Strategy, max = 6) {
+    const initial = Date.now()
 
-    for (const answer of possible) {
+    for (let i = 0; i < possible.length; i++) {
+        const answer = possible[i]!
+
         let remaining = possible
         let rounds = 0
 
@@ -96,16 +75,31 @@ function statGuesses(possible: Set, strat: Strategy, max = 6): number[] {
             rounds = Infinity
         }
 
-        ret.push(rounds)
+        console.log(
+            `${wordToString(answer)}: ${rounds}, // ${Date.now() - initial} est. ${Math.round(((Date.now() - initial) * ((possible.length - i) / i)) / 1000)}`,
+        )
+    }
+}
+
+// Strategy which minimizes the variance of the partition sizes.
+//
+// Takes `O(|possible|²)` time.
+function stratMinimizeVarPartitionSize(possible: Set): Word {
+    let min = varPartitionSize(possible, possible[0]!)
+    let ret = possible[0]!
+
+    for (let i = 1; i < possible.length; i++) {
+        const guess = possible[i]!
+        const size = varPartitionSize(possible, guess)
+        if (size < min) {
+            min = size
+            ret = guess
+        }
     }
 
     return ret
 }
 
 console.time()
-const guesses = statGuesses(SOLUTIONS, stratMinimizePartitionSize)
+statGuesses(SOLUTIONS, stratMinimizeVarPartitionSize)
 console.timeEnd()
-
-console.log(
-    Object.fromEntries(guesses.map((x, i) => [wordToString(SOLUTIONS[i]!), x])),
-)
