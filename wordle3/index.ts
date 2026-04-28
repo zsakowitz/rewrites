ffecec9e83539aa83be2c0a07f46ad8563912e72
sakawi <65452710+zsakowitz@users.noreply.github.com>
2026-04-28 11:38:45 -0700

    delete strat

--- wordle3/index.ts
+++ wordle3/index.ts
@@ -1,5 +1,4 @@
 import "../lib"
-import { randomItem } from "../random-item"
 import { solutions } from "./data"
 import { check, wordFromString, type Score, type Word } from "./score"
 
@@ -37,35 +36,3 @@ function filter(possible: Set, answer: Word, guess: Word): Set {
 
     return ret
 }
-
-type Strategy = (possible: Set, depth: number) => Word
-
-function estMaxGuesses(
-    possible: Set,
-    depth: number,
-    strategy: Strategy,
-): number {
-    if (possible.length == 1) {
-        return 0
-    }
-
-    const guess = strategy(possible, depth)
-
-    const largestPartition = Array.from(partition(possible, guess)).reduce(
-        (a, b) => (a[1].length > b[1].length ? a : b),
-    )[1]
-
-    return 1 + estMaxGuesses(largestPartition, depth, strategy)
-}
-
-console.time()
-console.log(
-    estMaxGuesses(SOLUTIONS, 0, (possible, depth) => {
-        if (depth == 0) {
-            return possible.minBy((guess) => estMaxGuesses())
-        } else {
-            return randomItem(possible)!
-        }
-    }),
-)
-console.timeEnd()
