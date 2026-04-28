e2335d4e0d82b91d2fa642f3ed943bfc804e8e89
sakawi <65452710+zsakowitz@users.noreply.github.com>
2026-04-28 08:27:59 -0700

    remove stuff

--- wordle3/index.ts
+++ wordle3/index.ts
@@ -1,5 +1,5 @@
 import { solutions } from "./data"
-import { check, wordFromString, wordToString, type Word } from "./score"
+import { check, wordFromString, type Word } from "./score"
 
 // We use `Set` to refer to an inhabited (nonempty) array of words. They must not
 // contain duplicates.
@@ -7,48 +7,14 @@ type Set = readonly Word[]
 
 const SOLUTIONS: Set = solutions.map(wordFromString)
 
-function partition(possible: Set, guess: Word): Set[] {
+function partition(possible: Set, guess: Word): Record<number, Set> {
     const partitions: Record<number, Word[]> = Object.create(null)
 
     for (const solution of possible) {
         ;(partitions[check(solution, guess)] ??= []).push(solution)
     }
 
-    return Object.values(partitions)
-}
-
-function sqsum(x: number[]): number {
-    return x.reduce((a, b) => a + b * b, 0)
-}
-
-function min1(possible: Set): readonly [Word, number] {
-    return possible
-        .map(
-            (guess) =>
-                [
-                    guess,
-                    sqsum(partition(possible, guess).map((x) => x.length)),
-                ] as const,
-        )
-        .reduce((a, b) => (a[1] < b[1] ? a : b))
-}
-
-function min2(possible: Set): readonly [Word, number] {
-    return possible
-        .map(
-            (guess) =>
-                [
-                    guess,
-                    partition(possible, guess)
-                        .map((p) => min1(p))
-                        .reduce((a, b) => (a[1] > b[1] ? a : b))[1],
-                ] as const,
-        )
-        .reduce((a, b) => (a[1] < b[1] ? a : b))
-}
-
-function strat(possible: Set): Word {
-    return min2(possible)[0]
+    return partitions
 }
 
 function filter(possible: Set, answer: Word, guess: Word): Set {
@@ -64,51 +30,3 @@ function filter(possible: Set, answer: Word, guess: Word): Set {
 
     return ret
 }
-
-const MAX_GUESSES = 6
-
-function guessesPerWord(possible: Set, initialGuess: Word): number[] {
-    return possible.map((answer): number => {
-        let rounds = 1
-        let remaining = filter(possible, answer, initialGuess)
-
-        while (remaining.length > 1 && rounds < MAX_GUESSES) {
-            remaining = filter(remaining, answer, strat(remaining))
-            rounds++
-        }
-
-        if (remaining.length > 1) {
-            return 99
-        }
-
-        return rounds
-    })
-}
-
-function logStats(possible: Set, initialGuess: Word) {
-    const guesses = guessesPerWord(possible, initialGuess)
-    const sorted = guesses.toSorted((a, b) => a - b)
-
-    const avg = guesses.reduce((a, b) => a + b, 0) / guesses.length
-    const max = guesses.reduce((a, b) => Math.max(a, b), 0)
-
-    console.log({
-        avg,
-        p25: sorted[Math.floor(sorted.length / 4)],
-        p50: sorted[Math.floor(sorted.length / 2)],
-        p75: sorted[Math.floor(sorted.length * 0.75)],
-        max,
-        tooLong: guesses
-            .map((x, i) => [x, i] as const)
-            .filter((x) => x[0] == 99)
-            .map((x) => wordToString(possible[x[1]]!)),
-    })
-}
-
-// const parts = partition(SOLUTIONS, strat(SOLUTIONS))
-//
-// console.log(parts.map((x) => x.length).sort((a, b) => a - b))
-
-console.time()
-console.log(strat(SOLUTIONS))
-console.timeEnd()
