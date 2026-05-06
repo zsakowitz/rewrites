3c4bbcfc169d8478b99aef005c7f6a25aa59bf63
sakawi <65452710+zsakowitz@users.noreply.github.com>
2026-05-05 17:16:43 -0700

    Start on Notebook

---
+++ notebook/asset/env.d.ts
@@ -0,0 +1,9 @@
+declare module "./asset/*.png" {
+    const x: string
+    export default x
+}
+
+declare module "./asset/*.jpeg" {
+    const x: string
+    export default x
+}
---
+++ notebook/index.ts
@@ -0,0 +1,106 @@
+import test1 from "./asset/test-1.png"
+import test2 from "./asset/test-2.png"
+import test3 from "./asset/test-3.jpeg"
+import test4 from "./asset/test-4.jpeg"
+
+const test = [
+    test2,
+    test1,
+    test3,
+    test4,
+    test3,
+    test1,
+    test4,
+    test2,
+    test2,
+    test1,
+    test4,
+    test3,
+    test1,
+    test2,
+    test4,
+    test3,
+    test2,
+    test4,
+    test1,
+    test4,
+    test2,
+    test4,
+    test3,
+    test3,
+    test1,
+    test4,
+    test1,
+    test3,
+    test2,
+    test3,
+    test1,
+    test2,
+]
+
+type Attr = string | null | undefined | Record<string, string | number>
+type Child = string | HTMLElement
+
+function h<K extends keyof HTMLElementTagNameMap>(
+    tagName: K,
+    attr: Attr,
+    ...children: Child[]
+): HTMLElementTagNameMap[K] {
+    const el = document.createElement(tagName)
+
+    if (typeof attr == "string") {
+        el.className = attr
+    } else if (attr != null) {
+        for (const key in attr) {
+            el.setAttribute(key, "" + attr[key]!)
+        }
+    }
+
+    el.append.apply(el, children)
+
+    return el
+}
+
+function div(attr: Attr, ...children: Child[]) {
+    return h("div", attr, ...children)
+}
+
+function SidebarRightImage(src: string, selected: boolean) {
+    return h(
+        "li",
+        "relative" + (selected ? " ring-4 ring-accent-500 rounded-md" : ""),
+        h("img", {
+            class: "rounded-md" + (selected ? "" : " opacity-70"),
+            src,
+        }),
+    )
+}
+
+function MainImage(src: string) {
+    const img1 = h("img", {
+        class: "absolute -top-2 -left-2 w-[calc(100%+16px)] max-w-none h-[calc(100%+16px)] object-cover opacity-30 blur-xs",
+        src,
+    })
+
+    const img2 = h("img", { class: "relative m-auto max-h-full", src })
+
+    return div("bg-white", img1, div("flex relative h-full p-4", img2))
+}
+
+function Main() {
+    return div(
+        "min-h-dvh grid grid-cols-[240px_1fr_240px] grid-rows-[2rem_1fr_3rem_6rem] *:contain-strict",
+        div("bg-gray-300 col-span-3 border-b border-gray-500"),
+        div("bg-gray-200 row-span-3 border-r border-gray-500"),
+        MainImage(test1),
+        h(
+            "ul",
+            "bg-gray-200 row-span-3 border-l border-gray-500 flex flex-wrap p-2 gap-2 overflow-auto",
+            ...test.map((x, i) => SidebarRightImage(x, i == 1)),
+        ),
+        div("bg-gray-100 border-t border-gray-500"),
+        div("bg-gray-100 border-t border-gray-500"),
+    )
+}
+
+document.body.appendChild(Main())
