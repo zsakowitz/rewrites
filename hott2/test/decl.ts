f7d27f48edd717fd4455c944f6b181008a95367d
sakawi <65452710+zsakowitz@users.noreply.github.com>
2026-05-15 22:50:24 -0700

    start on expr-sub

--- hott2/test/decl.ts
+++ hott2/test/decl.ts
@@ -8,7 +8,7 @@ export function assert(cond: boolean) {
 
 export function eq<T>(actual: T, expected: T) {
     if (actual !== expected) {
-        throw new Error(`${reset}❌ found ${actual}, expected ${expected}`)
+        throw new Error(`${reset}❌\n    found ${actual},\n expected ${expected}`)
     }
 }
 
@@ -17,6 +17,6 @@ const ANSI = /\x1b\[\d+(?:,\d+)*m/g
 /** Strips ANSI character sequences from `x`, then checks equality to `y`. */
 export function eqAnsi(actual: string, expected: string) {
     if (actual.replaceAll(ANSI, "") !== expected) {
-        throw new Error(`${reset}❌ found ${actual}${reset}, expected ${expected}`)
+        throw new Error(`${reset}❌\n    found ${actual}${reset},\n expected ${expected}`)
     }
 }
--- hott2/test/expr-sub.ts
+++ hott2/test/expr-sub.ts
@@ -65,3 +65,35 @@ eqAnsi(
     ),
     "λ. λb. b b (λc. c)",
 )
+
+eqAnsi(
+    printExpr(
+        [],
+        0,
+        // λa. λb.
+        Fn(
+            2,
+            sub(
+                Apply(Var(0), Var(2), Func(Apply(Var(0), Var(1)))), // substituting into λc. c a (λd. d c)
+                Var(0), // with b
+            ),
+        ),
+    ),
+    "λa. λb. b a (λc. c b)",
+)
+
+eqAnsi(
+    printExpr(
+        [],
+        0,
+        // λa. λb.
+        Fn(
+            2,
+            sub(
+                Apply(Var(0), Var(2), Func(Apply(Var(0), Var(1)))), // substituting into λc. c a (λd. d c)
+                Apply(Var(0), Var(1)), // with b a
+            ),
+        ),
+    ),
+    "λa. λb. b a a (λc. c (b a))",
+)
